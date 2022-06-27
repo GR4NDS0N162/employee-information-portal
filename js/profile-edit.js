c297cd43b653c9ad26788d7091ac41e90fe38549
Kirill Vnukov <kirillgrandson@gmail.com>
2022-06-27 17:48:10 +0300

    OGGYKIDS-201836: Get elements from an html document

---
+++ js/profile-edit.js
@@ -0,0 +1,7 @@
+const password_form = document.getElementById('password-form');
+const password_current_field = document.getElementById('password-current-field');
+const password_current_error = document.getElementById('password-current-error');
+const password_new_field = document.getElementById('password-new-field');
+const password_new_error = document.getElementById('password-new-error');
+const password_check_field = document.getElementById('password-check-field');
+const password_check_error = document.getElementById('password-check-error');
