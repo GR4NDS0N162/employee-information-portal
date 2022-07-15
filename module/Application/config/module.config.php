e0400466e94e112dafdc6a3bff7b41a6aa99489f
Kirill Vnukov <kirillgrandson@gmail.com>
2022-07-15 16:04:04 +0300

    OGGYKIDS-203159: Create an individual layout for the login page

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -215,6 +215,7 @@ return [
         'exception_template'       => 'error/index',
         'template_map' => [
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/login'            => __DIR__ . '/../view/layout/login.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
