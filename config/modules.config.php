1405ac23da25724f4203d805cccdd47a632ba5a6
Kirill Vnukov <kirillgrandson@gmail.com>
2022-07-15 13:09:48 +0300

    OGGYKIDS-203159: Install laminas-navigation

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,6 +6,7 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Navigation',
     'Laminas\Router',
     'Laminas\Validator',
     'Application',
