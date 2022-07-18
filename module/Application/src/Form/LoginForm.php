5733d0b257209d8d6a2bec5f732569c16934ff77
Kirill Vnukov <kirillgrandson@gmail.com>
2022-07-18 12:58:30 +0300

    OGGYKIDS-203159: Create a login form class

---
+++ module/Application/src/Form/LoginForm.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Form;
+
+use Laminas\Form\Form;
+
+class LoginForm extends Form
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
