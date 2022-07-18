07874db611dfc6d3c6985082900c98b07ea2b0e5
Kirill Vnukov <kirillgrandson@gmail.com>
2022-07-18 13:01:58 +0300

    OGGYKIDS-203159: Add fields to the login form

--- module/Application/src/Form/LoginForm.php
+++ module/Application/src/Form/LoginForm.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Application\Form;
 
+use Laminas\Form\Element\Email;
+use Laminas\Form\Element\Password;
+use Laminas\Form\Element\Submit;
 use Laminas\Form\Form;
 
 class LoginForm extends Form
@@ -11,5 +14,27 @@ class LoginForm extends Form
     public function __construct()
     {
         parent::__construct();
+
+        $this->add([
+            'name' => 'email',
+            'type' => Email::class,
+            'options' => [
+                'label' => 'E-mail',
+            ],
+        ]);
+        $this->add([
+            'name' => 'password',
+            'type' => Password::class,
+            'options' => [
+                'label' => 'Пароль',
+            ],
+        ]);
+        $this->add([
+            'name' => 'submit',
+            'type' => Submit::class,
+            'attributes' => [
+                'value' => 'Submit',
+            ],
+        ]);
     }
 }
