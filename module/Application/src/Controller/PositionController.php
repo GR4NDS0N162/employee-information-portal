986d80a261d6a137e4564c7359d798715b9ad6e1
Kirill Vnukov <kirillgrandson@gmail.com>
2022-07-21 14:20:23 +0300

    OGGYKIDS-203159: Create a position editing form

--- module/Application/src/Controller/PositionController.php
+++ module/Application/src/Controller/PositionController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Application\Controller;
 
+use Application\Entity\Position;
+use Application\Form\PositionForm;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
 
@@ -15,8 +17,22 @@ class PositionController extends AbstractActionController
 
         $headTitleName = 'Управление должностями (Администратор)';
 
+        $positionForm = new PositionForm();
+        $position = new Position();
+        $positionForm->bind($position);
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+            $positionForm->setData($request->getPost());
+
+            if ($positionForm->isValid()) {
+                var_dump($position);
+            }
+        }
+
         $viewModel->setVariables([
             'headTitleName' => $headTitleName,
+            'positionForm'  => $positionForm,
         ]);
 
         return $viewModel;
