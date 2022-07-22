1f35619e2bcf2344532b9cd7ce3d9900c054a25b
Kirill Vnukov <kirillgrandson@gmail.com>
2022-07-22 18:05:37 +0300

    OGGYKIDS-203159: Create the PositionButtonFieldset array to be included in the form

---
+++ module/Application/src/Element/PositionButtonFieldset.php
@@ -0,0 +1,11 @@
+<?php
+
+use Application\Fieldset\PositionButtonFieldset;
+
+return [
+    'name' => 'buttons',
+    'type' => PositionButtonFieldset::class,
+    'attributes' => [
+        'class' => 'btn-group',
+    ],
+];
