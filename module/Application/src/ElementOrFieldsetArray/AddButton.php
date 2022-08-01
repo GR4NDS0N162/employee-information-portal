753059e7dab208b8903016b42eec4a55f6cdbf43
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-01 20:27:42 +0300

    OGGYKIDS-203159: Create an array of add button

---
+++ module/Application/src/ElementOrFieldsetArray/AddButton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\ElementOrFieldsetArray;
+
+use Laminas\Form\Element\Button;
+
+return [
+    'name' => 'add-button',
+    'type' => Button::class,
+    'attributes' => [
+        'class' => 'btn btn-outline-primary w-100',
+    ],
+];
