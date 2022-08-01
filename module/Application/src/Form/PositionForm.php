3dd0dc16a77d3ec5df2dbab94ed44ebc931b2aa9
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-01 20:27:10 +0300

    OGGYKIDS-203159: Add the add button to the form

--- module/Application/src/Form/PositionForm.php
+++ module/Application/src/Form/PositionForm.php
@@ -14,8 +14,14 @@ class PositionForm extends Form
             'class' => 'row g-3',
         ]);
 
+        $this->add(include __DIR__ . '/../ElementOrFieldsetArray/AddButton.php');
         $this->add(include __DIR__ . '/../ElementOrFieldsetArray/PositionCollection.php');
         $this->add(include __DIR__ . '/../ElementOrFieldsetArray/SubmitButton.php');
+
+        $this->get('add-button')
+            ->setOptions([
+                'label' => 'Добавить новую должность',
+            ]);
 
         $this->get('submit-button')
             ->setAttributes([
