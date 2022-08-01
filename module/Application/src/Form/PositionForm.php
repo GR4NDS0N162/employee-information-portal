6e4fe3353f9dad84387bbde742d4c3dace8c1d32
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-01 19:40:44 +0300

    OGGYKIDS-203159: Customize the form

--- module/Application/src/Form/PositionForm.php
+++ module/Application/src/Form/PositionForm.php
@@ -10,7 +10,19 @@ class PositionForm extends Form
     {
         parent::__construct();
 
+        $this->setAttributes([
+            'class' => 'row g-3',
+        ]);
+
         $this->add(include __DIR__ . '/../ElementOrFieldsetArray/PositionCollection.php');
         $this->add(include __DIR__ . '/../ElementOrFieldsetArray/SubmitButton.php');
+
+        $this->get('submit-button')
+            ->setAttributes([
+                'class' => 'btn btn-lg btn-outline-success w-100',
+            ])
+            ->setOptions([
+                'label' => 'Сохранить изменения',
+            ]);
     }
 }
