07f92d40d0ad3287af0835c71f77659ac4f8483c
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-08 17:14:36 +0300

    OGGYKIDS-203159: Add and display dialogs and their filtering form

--- module/Application/src/Controller/UserController.php
+++ module/Application/src/Controller/UserController.php
@@ -116,6 +116,48 @@ class UserController extends AbstractActionController
 
         $this->layout()->setVariable('headTitleName', $headTitleName);
 
+        $dialogs = [
+            [
+                'userId' => 1,
+                'hasDialog' => true,
+                'photo' => 'https://picsum.photos/' . random_int(100, 999),
+                'fullname' => 'Зубенко Михаил Петрович',
+                'position' => 'Уборщик',
+                'gender' => 'Мужской',
+                'age' => 47,
+            ],
+            [
+                'userId' => 2,
+                'hasDialog' => true,
+                'photo' => 'https://picsum.photos/' . random_int(100, 999),
+                'fullname' => 'Егоров Владимир Егорович',
+                'position' => 'Бухгалтер',
+                'gender' => 'Мужской',
+                'age' => 31,
+            ],
+            [
+                'userId' => 3,
+                'hasDialog' => false,
+                'photo' => 'https://picsum.photos/' . random_int(100, 999),
+                'fullname' => 'Мельникова Алёна Вадимовна',
+                'position' => 'Юрист',
+                'gender' => 'Женский',
+                'age' => 23,
+            ],
+            [
+                'userId' => 4,
+                'hasDialog' => false,
+                'photo' => 'https://picsum.photos/' . random_int(100, 999),
+                'fullname' => 'Тимофеева Вероника Денисовна',
+                'position' => 'Менеджер',
+                'gender' => 'Женский',
+                'age' => 36,
+            ],
+        ];
+
+        $viewModel->setVariable('dialogs', $dialogs);
+        $viewModel->setVariable('dialogFilterForm', new Form\DialogFilterForm());
+
         return $viewModel;
     }
 
