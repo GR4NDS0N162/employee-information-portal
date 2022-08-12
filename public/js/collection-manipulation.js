4ddc0b84f4af4e6a5b407597ec7cfb7b68f21c65
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-12 17:13:16 +0300

    OGGYKIDS-203159: Set up the list manipulation code

--- public/js/collection-manipulation.js
+++ public/js/collection-manipulation.js
@@ -1,12 +1,23 @@
 function add_item(button)
 {
     let formId = button.closest('form').getAttribute('id');
-    let template = $(`#${formId} > div > div > span`).data('template');
-    let container = $(`#${formId} > div > [name="list"]`);
+    let container = $(`#${formId} [name="list"]`);
+    let template = $(`#${formId} [name="list"] span`).data('template');
 
-    let lastInput = $(`#${formId} > div > [name="list"] > .item > div > input`).last()[0];
-    let newIndex = (lastInput) ? parseInt(lastInput.getAttribute('name').match(/[0-9]+(?=])/)[0]) + 1 : 0;
-    container.append(template.replace(/__index__/g, newIndex));
+    if (!container[0].hasAttribute('current-index'))
+        calculateIndex(formId, container[0]);
+
+    let currentIndex = parseInt(container[0].getAttribute('current-index'));
+    container.append(template.replace(/__index__/g, currentIndex));
+
+    container[0].setAttribute('current-index', ++currentIndex);
+}
+
+function calculateIndex(formId, containerElement)
+{
+    let lastInput = $(`#${formId} [name="list"] > .item:last-child input`)[0];
+    let currentIndex = (!lastInput) ? 0 : parseInt(lastInput.getAttribute('name').match(/\d+(?=])/)[0]) + 1;
+    containerElement.setAttribute('current-index', currentIndex);
 }
 
 const cantBeEmpty = ['edit-email-form'];
@@ -14,10 +25,25 @@ const cantBeEmpty = ['edit-email-form'];
 function delete_item(button)
 {
     let formId = button.closest('form').getAttribute('id');
-    let currentCount = $(`#${formId} > div > div > .item`).length;
+    let currentCount = $(`#${formId} [name="list"] .item`).length;
 
     if (!(currentCount === 1 && cantBeEmpty.indexOf(formId) !== -1))
         button.closest('.item').remove();
-    else
-        button.previousSibling.previousSibling.innerText = 'Список не может быть пустым.';
+    else {
+        let container = $(`#${formId} [name="list"]`);
+        let feedback = button.previousSibling.previousSibling;
+        let input = feedback.previousSibling.previousSibling;
+
+        feedback.innerText = 'Этот список не может быть пустым.';
+
+        if (!container[0].hasAttribute('current-index'))
+            calculateIndex(formId, container[0]);
+
+        let currentIndex = parseInt(container[0].getAttribute('current-index'));
+
+        input.value = '';
+        input.setAttribute('name', `list[${currentIndex}]`);
+
+        container[0].setAttribute('current-index', ++currentIndex);
+    }
 }
