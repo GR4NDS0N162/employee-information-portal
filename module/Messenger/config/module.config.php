5a7cccfeaef070e0e4bd9529f6a75ca141145c07
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-19 14:42:00 +0300

    OGGYKIDS-204246: Create a DialogListController

--- module/Messenger/config/module.config.php
+++ module/Messenger/config/module.config.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 namespace Messenger;
 
 use Laminas\Router\Http\Literal;
+use Laminas\ServiceManager\Factory\InvokableFactory;
 
 return [
-    'router' => [
+    'controllers' => [
+        'factories' => [
+            Controller\DialogListController::class => InvokableFactory::class,
+        ],
+    ],
+    'router'      => [
         'routes' => [
             'messenger' => [
                 'type'    => Literal::class,
---
+++ module/Messenger/src/Controller/DialogListController.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Messenger\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+
+class DialogListController extends AbstractActionController
+{
+}
