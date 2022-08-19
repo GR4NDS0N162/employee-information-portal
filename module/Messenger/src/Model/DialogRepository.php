634c907fe099d4a55d6aa98805bc1ff735514f4e
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-19 14:52:39 +0300

    OGGYKIDS-204246: Create a DialogRepository

---
+++ module/Messenger/src/Model/DialogRepository.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Messenger\Model;
+
+class DialogRepository implements DialogRepositoryInterface
+{
+    /**
+     * @return mixed
+     */
+    public function findAllDialogs()
+    {
+        // TODO: Implement findAllDialogs() method.
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function findDialog($id)
+    {
+        // TODO: Implement findDialog() method.
+    }
+}
