23785c1cf66fae4e4a73a06fea3b907299d300f9
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-19 15:11:58 +0300

    OGGYKIDS-204246: Implement dependency on DialogRepositoryInterface

--- module/Messenger/src/Controller/DialogListController.php
+++ module/Messenger/src/Controller/DialogListController.php
@@ -4,7 +4,23 @@ declare(strict_types=1);
 namespace Messenger\Controller;
 
 use Laminas\Mvc\Controller\AbstractActionController;
+use Messenger\Model\DialogRepositoryInterface;
 
+/**
+ *
+ */
 class DialogListController extends AbstractActionController
 {
+    /**
+     * @var DialogRepositoryInterface
+     */
+    private DialogRepositoryInterface $dialogRepository;
+
+    /**
+     * @param DialogRepositoryInterface $dialogRepository
+     */
+    public function __construct(DialogRepositoryInterface $dialogRepository)
+    {
+        $this->dialogRepository = $dialogRepository;
+    }
 }
