bfa596bb936ef0c0606118d3e1157af5de8b48fa
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-19 16:37:04 +0300

    OGGYKIDS-204246: Implement interaction with the database

--- module/Messenger/src/Model/LaminasDbSqlRepository.php
+++ module/Messenger/src/Model/LaminasDbSqlRepository.php
@@ -2,6 +2,11 @@
 
 namespace Messenger\Model;
 
+use Laminas\Db\Adapter\Driver\ResultInterface;
+use Laminas\Db\ResultSet\HydratingResultSet;
+use Laminas\Db\Sql\Sql;
+use Laminas\Hydrator\ReflectionHydrator;
+
 class LaminasDbSqlRepository implements DialogRepositoryInterface
 {
     private $db;
@@ -13,7 +18,21 @@ class LaminasDbSqlRepository implements DialogRepositoryInterface
 
     public function findAllDialogs()
     {
-        // TODO: Implement findAllDialogs() method.
+        $sql = new Sql($this->db);
+        $select = $sql->select('dialog');
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $result = $statement->execute();
+
+        if (!$result instanceof ResultInterface || !$result->isQueryResult()) {
+            return [];
+        }
+
+        $resultSet = new HydratingResultSet(
+            new ReflectionHydrator(),
+            new Dialog()
+        );
+        $resultSet->initialize($result);
+        return $resultSet;
     }
 
     public function findDialog($id)
