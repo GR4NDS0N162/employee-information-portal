2c9197411c1c79dcb399f6dfacafdddcdcdbd85e
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-19 15:06:10 +0300

    OGGYKIDS-204246: Add the id to the dialog entity

--- module/Messenger/src/Model/Dialog.php
+++ module/Messenger/src/Model/Dialog.php
@@ -3,6 +3,29 @@ declare(strict_types=1);
 
 namespace Messenger\Model;
 
+/**
+ *
+ */
 class Dialog
 {
+    /**
+     * @var int
+     */
+    private int $id;
+
+    /**
+     * @param int|null $id
+     */
+    public function __construct(int $id = null)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
 }
