3b63bebe3ec74e640bc038555d8ef1a6f0637894
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-19 15:03:40 +0300

    OGGYKIDS-204246: Create a variable that stores test data about dialogs

--- module/Messenger/src/Model/DialogRepository.php
+++ module/Messenger/src/Model/DialogRepository.php
@@ -5,6 +5,24 @@ namespace Messenger\Model;
 
 class DialogRepository implements DialogRepositoryInterface
 {
+    private $data = [
+        1 => [
+            'id' => 1,
+        ],
+        2 => [
+            'id' => 2,
+        ],
+        3 => [
+            'id' => 3,
+        ],
+        4 => [
+            'id' => 4,
+        ],
+        5 => [
+            'id' => 5,
+        ],
+    ];
+
     /**
      * @return Dialog[]
      */
