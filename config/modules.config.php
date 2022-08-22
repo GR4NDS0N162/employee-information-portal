dfb339e93a26de2c02ee190ed36c2533a3fd3c1b
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-22 22:04:27 +0300

    OGGYKIDS-204246: Create a new module for the home page

--- config/modules.config.php
+++ config/modules.config.php
@@ -18,4 +18,5 @@ return [
     'Application',
     'Messenger',
     'User',
+    'Home',
 ];
---
+++ module/Home/config/module.config.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Home;
+
+return [
+    'service_manager' => [
+        'aliases'   => [
+        ],
+        'factories' => [
+        ],
+    ],
+    'controllers'     => [
+        'factories' => [
+        ],
+    ],
+    'router'          => [
+        'routes' => [
+        ],
+    ],
+    'view_manager'    => [
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+];
