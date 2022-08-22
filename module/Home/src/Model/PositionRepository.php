b8f892e1abc85cff6c20b1d3048d8e2fc791c10a
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-23 00:05:29 +0300

    OGGYKIDS-204246: Implement the findPosition method in the PositionRepository class

--- module/Home/src/Model/PositionRepository.php
+++ module/Home/src/Model/PositionRepository.php
@@ -2,8 +2,13 @@
 
 namespace Home\Model;
 
+use InvalidArgumentException;
 use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Adapter\Driver\ResultInterface;
+use Laminas\Db\ResultSet\HydratingResultSet;
+use Laminas\Db\Sql\Sql;
 use Laminas\Hydrator\HydratorInterface;
+use RuntimeException;
 
 class PositionRepository implements PositionRepositoryInterface
 {
@@ -41,6 +46,31 @@ class PositionRepository implements PositionRepositoryInterface
 
     public function findPosition($id)
     {
-        // TODO: Implement findPosition() method.
+        $sql = new Sql($this->db);
+        $select = $sql->select('position');
+        $select->where(['id = ?' => $id]);
+
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $result = $statement->execute();
+
+        if (!$result instanceof ResultInterface || !$result->isQueryResult()) {
+            throw new RuntimeException(sprintf(
+                'Failed retrieving position with identifier "%d"; unknown database error.',
+                $id
+            ));
+        }
+
+        $resultSet = new HydratingResultSet($this->hydrator, $this->positionPrototype);
+        $resultSet->initialize($result);
+        $position = $resultSet->current();
+
+        if (!$position) {
+            throw new InvalidArgumentException(sprintf(
+                'Position with identifier "%d" not found.',
+                $id
+            ));
+        }
+
+        return $position;
     }
 }
