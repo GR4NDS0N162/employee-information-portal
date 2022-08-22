fe31567302ce172321582b531b0a04e9f7813747
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-22 12:26:48 +0300

    OGGYKIDS-204246: Implement dependency in ProfileController

--- module/User/src/Controller/ProfileController.php
+++ module/User/src/Controller/ProfileController.php
@@ -3,7 +3,30 @@
 namespace User\Controller;
 
 use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
+use User\Model\ProfileRepositoryInterface;
 
 class ProfileController extends AbstractActionController
 {
+    /**
+     * @var ProfileRepositoryInterface
+     */
+    private $profileRepository;
+
+    /**
+     * @param ProfileRepositoryInterface $profileRepository
+     */
+    public function __construct($profileRepository)
+    {
+        $this->profileRepository = $profileRepository;
+    }
+
+    public function indexAction()
+    {
+        $userId = 1;
+
+        return new ViewModel([
+            'profile' => $this->profileRepository->findProfile($userId),
+        ]);
+    }
 }
--- module/User/src/Factory/ProfileControllerFactory.php
+++ module/User/src/Factory/ProfileControllerFactory.php
@@ -5,11 +5,14 @@ namespace User\Factory;
 use Interop\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use User\Controller\ProfileController;
+use User\Model\ProfileRepositoryInterface;
 
 class ProfileControllerFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
-        return new ProfileController();
+        return new ProfileController(
+            $container->get(ProfileRepositoryInterface::class)
+        );
     }
 }
