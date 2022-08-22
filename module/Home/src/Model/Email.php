65c63f3f5d3a684048a09c4db4bafe926f2eeb5c
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-22 23:21:27 +0300

    OGGYKIDS-204246: Create an email entity

---
+++ module/Home/src/Model/Email.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Home\Model;
+
+class Email
+{
+    /**
+     * @var non-empty-string
+     */
+    private $address;
+
+    /**
+     * @var positive-int|null
+     */
+    private $userId;
+
+    /**
+     * @param string $address
+     * @param int|null $userId
+     */
+    public function __construct($address, $userId = null)
+    {
+        $this->address = $address;
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+}
