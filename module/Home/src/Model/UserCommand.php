1f347a688032e77cec428b8839a6448b7f46610f
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-23 00:30:32 +0300

    OGGYKIDS-204246: Create a UserCommand class

---
+++ module/Home/src/Model/UserCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Home\Model;
+
+class UserCommand implements UserCommandInterface
+{
+    public function insertUser($user)
+    {
+        // TODO: Implement insertUser() method.
+    }
+}
