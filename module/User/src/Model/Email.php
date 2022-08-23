489f096f59b0b301659a8958d146d46c7f096e67
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-23 17:38:36 +0300

    OGGYKIDS-204246: Add fields, constructor and getters to entities

--- module/User/src/Model/Email.php
+++ module/User/src/Model/Email.php
@@ -4,4 +4,39 @@ namespace User\Model;
 
 class Email
 {
+    /**
+     * @var non-empty-string
+     */
+    private $address;
+
+    /**
+     * @var positive-int|null
+     */
+    private $userId;
+
+    /**
+     * @param string $address
+     * @param int|null $userId
+     */
+    public function __construct($address, $userId = null)
+    {
+        $this->address = $address;
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
 }
--- module/User/src/Model/Phone.php
+++ module/User/src/Model/Phone.php
@@ -4,4 +4,39 @@ namespace User\Model;
 
 class Phone
 {
+    /**
+     * @var non-empty-string
+     */
+    private $number;
+
+    /**
+     * @var positive-int|null
+     */
+    private $userId;
+
+    /**
+     * @param string $number
+     * @param int|null $userId
+     */
+    public function __construct($number, $userId = null)
+    {
+        $this->number = $number;
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
 }
