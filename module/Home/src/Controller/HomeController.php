9ec90b51ae373e27b9d6f2c7a2b79250df61d84f
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-23 14:55:26 +0300

    OGGYKIDS-204246: Get the request and make an attempt to insert

--- module/Home/src/Controller/HomeController.php
+++ module/Home/src/Controller/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace Home\Controller;
 
+use Exception;
 use Home\Form\LoginForm;
 use Home\Form\RecoverForm;
 use Home\Form\SignUpForm;
+use Home\Model\Email;
 use Home\Model\EmailRepositoryInterface;
 use Home\Model\PositionRepositoryInterface;
+use Home\Model\User;
 use Home\Model\UserCommandInterface;
 use Home\Model\UserRepositoryInterface;
 use Laminas\Mvc\Controller\AbstractActionController;
@@ -92,6 +95,33 @@ class HomeController extends AbstractActionController
 
     public function signUpAction()
     {
+        $request = $this->getRequest();
+
+        if (!$request->isPost())
+            return $this->redirect()->toRoute('home');
+
+        $this->signUpForm->setData($request->getPost());
+
+        if (!$this->signUpForm->isValid())
+            return $this->redirect()->toRoute('home');
+
+        $data = $this->signUpForm->getData();
+
+        $email = new Email($data['email']);
+        $user = new User(
+            $data['new-password'],
+            $data['position'],
+            $data['new-password'],
+            date('Y-m-d H:i:s')
+        );
+
+        try {
+            $post = $this->userCommand->insertUser($user, $email);
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+
+        return $this->redirect()->toRoute('userview');
     }
 
     public function recoverAction()
