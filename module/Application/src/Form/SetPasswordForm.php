c9994c542576b7dafaced5cf9072a560023a5c7a
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-24 18:52:24 +0300

    OGGYKIDS-203159: Use an inline variable instead of a class field

--- module/Application/src/Form/SetPasswordForm.php
+++ module/Application/src/Form/SetPasswordForm.php
@@ -9,43 +9,6 @@ class SetPasswordForm extends Form
 {
     public const DEFAULT_NAME = 'set-password-form';
 
-    protected array $newPassword = [
-        'name'       => 'new-password',
-        'type'       => Element\Password::class,
-        'attributes' => [
-            'class'        => 'form-control',
-            'placeholder'  => 'qwerty123',
-            'required'     => 'required',
-            'autocomplete' => 'new-password',
-            'minlength'    => 8,
-            'maxlength'    => 32,
-            'pattern'      => '^(?=.*?[а-яa-z])(?=.*?[А-ЯA-Z])(?=.*?[0-9])(?=.*?[!"#\$%&\'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~])[а-яa-zА-ЯA-Z0-9!"#\$%&\'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~]*$',
-        ],
-        'options'    => [
-            'label'            => 'Новый пароль',
-            'label_attributes' => [
-                'class' => 'form-label',
-            ],
-        ],
-    ];
-
-    protected array $passwordCheck = [
-        'name'       => 'password-check',
-        'type'       => Element\Password::class,
-        'attributes' => [
-            'class'       => 'form-control',
-            'placeholder' => 'qwerty123',
-            'required'    => 'required',
-            'pattern'     => '',
-        ],
-        'options'    => [
-            'label'            => 'Подтверждение пароля',
-            'label_attributes' => [
-                'class' => 'form-label',
-            ],
-        ],
-    ];
-
     public function __construct($name = self::DEFAULT_NAME)
     {
         parent::__construct($name);
@@ -53,8 +16,43 @@ class SetPasswordForm extends Form
         $this->setAttribute('class', 'row gy-3 needs-validation');
         $this->setAttribute('novalidate', '');
 
-        $this->add($this->newPassword);
-        $this->add($this->passwordCheck);
+        $this->add([
+            'name'       => 'new-password',
+            'type'       => Element\Password::class,
+            'attributes' => [
+                'class'        => 'form-control',
+                'placeholder'  => 'qwerty123',
+                'required'     => 'required',
+                'autocomplete' => 'new-password',
+                'minlength'    => 8,
+                'maxlength'    => 32,
+                'pattern'      => '^(?=.*?[а-яa-z])(?=.*?[А-ЯA-Z])(?=.*?[0-9])(?=.*?[!"#\$%&\'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~])[а-яa-zА-ЯA-Z0-9!"#\$%&\'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~]*$',
+            ],
+            'options'    => [
+                'label'            => 'Новый пароль',
+                'label_attributes' => [
+                    'class' => 'form-label',
+                ],
+            ],
+        ]);
+
+        $this->add([
+            'name'       => 'password-check',
+            'type'       => Element\Password::class,
+            'attributes' => [
+                'class'       => 'form-control',
+                'placeholder' => 'qwerty123',
+                'required'    => 'required',
+                'pattern'     => '',
+            ],
+            'options'    => [
+                'label'            => 'Подтверждение пароля',
+                'label_attributes' => [
+                    'class' => 'form-label',
+                ],
+            ],
+        ]);
+
         $this->add([
             'name'       => 'submit-button',
             'type'       => Element\Button::class,
