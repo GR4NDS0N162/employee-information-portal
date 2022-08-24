91415843e46c11a4af9884811cb9cf53b124eb06
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-24 22:34:36 +0300

    OGGYKIDS-203159: Add elements to fieldsets

--- module/Application/src/Fieldset/EmailsFieldset.php
+++ module/Application/src/Fieldset/EmailsFieldset.php
@@ -2,8 +2,49 @@
 
 namespace Application\Fieldset;
 
+use Laminas\Form\Element\Button;
+use Laminas\Form\Element\Collection;
+use Laminas\Form\Element\Email;
 use Laminas\Form\Fieldset;
 
 class EmailsFieldset extends Fieldset
 {
+    public function init()
+    {
+        $this->add([
+            'name'       => 'add',
+            'type'       => Button::class,
+            'attributes' => [
+                'type'  => 'button',
+                'class' => 'btn btn-outline-primary w-100',
+            ],
+            'options'    => [
+                'label' => 'Добавить e-mail',
+            ],
+        ]);
+
+        $this->add([
+            'name'       => 'list',
+            'type'       => Collection::class,
+            'attributes' => [
+                'class' => 'row gy-3',
+            ],
+            'options'    => [
+                'target_element'         => [
+                    'type'       => Email::class,
+                    'attributes' => [
+                        'class'       => 'form-control',
+                        'placeholder' => 'name@example.com',
+                        'required'    => 'required',
+                        'pattern'     => '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$',
+                    ],
+                ],
+                'count'                  => 1,
+                'allow_add'              => true,
+                'allow_remove'           => true,
+                'should_create_template' => true,
+                'template_placeholder'   => '__index__',
+            ],
+        ]);
+    }
 }
--- module/Application/src/Fieldset/PhonesFieldset.php
+++ module/Application/src/Fieldset/PhonesFieldset.php
@@ -2,8 +2,49 @@
 
 namespace Application\Fieldset;
 
+use Laminas\Form\Element\Button;
+use Laminas\Form\Element\Collection;
+use Laminas\Form\Element\Tel;
 use Laminas\Form\Fieldset;
 
 class PhonesFieldset extends Fieldset
 {
+    public function init()
+    {
+        $this->add([
+            'name'       => 'add',
+            'type'       => Button::class,
+            'attributes' => [
+                'type'  => 'button',
+                'class' => 'btn btn-outline-primary w-100',
+            ],
+            'options'    => [
+                'label' => 'Добавить телефон',
+            ],
+        ]);
+
+        $this->add([
+            'name'       => 'list',
+            'type'       => Collection::class,
+            'attributes' => [
+                'class' => 'row gy-3',
+            ],
+            'options'    => [
+                'target_element'         => [
+                    'type'       => Tel::class,
+                    'attributes' => [
+                        'class'       => 'form-control',
+                        'placeholder' => '+7xxxxxxxxxx',
+                        'required'    => 'required',
+                        'pattern'     => '^\+7[0-9]{10}$',
+                    ],
+                ],
+                'count'                  => 0,
+                'allow_add'              => true,
+                'allow_remove'           => true,
+                'should_create_template' => true,
+                'template_placeholder'   => '__index__',
+            ],
+        ]);
+    }
 }
