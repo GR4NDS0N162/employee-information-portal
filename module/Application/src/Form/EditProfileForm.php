c508cbae20435ecbde18a0dd73cee27835cbd388
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-24 21:15:36 +0300

    OGGYKIDS-203159: Replace all elements with one Fieldset

--- module/Application/src/Form/EditProfileForm.php
+++ module/Application/src/Form/EditProfileForm.php
@@ -2,148 +2,30 @@
 
 namespace Application\Form;
 
-use Application\Form\Options\GenderOptions;
-use Laminas\Form\Element;
+use Application\Fieldset\ProfileFieldset;
+use Laminas\Form\Element\Submit;
 use Laminas\Form\Form;
 
 class EditProfileForm extends Form
 {
-    public const DEFAULT_NAME = 'edit-profile-form';
-
-    public function __construct($name = self::DEFAULT_NAME)
+    public function init()
     {
-        parent::__construct($name);
-
-        $this->setAttribute('class', 'row g-3');
-
         $this->add([
-            'name'       => 'photo',
-            'type'       => Element\File::class,
-            'attributes' => [
-                'class'  => 'form-control',
-                'accept' => 'image/*',
-            ],
-            'options'    => [
-                'label'            => 'Фото',
-                'label_attributes' => [
-                    'class' => 'form-label',
-                ],
-            ],
+            'name' => 'profile',
+            'type' => ProfileFieldset::class,
         ]);
 
         $this->add([
-            'name'       => 'surname',
-            'type'       => Element\Text::class,
+            'name'       => 'submit',
+            'type'       => Submit::class,
             'attributes' => [
-                'class'       => 'form-control',
-                'placeholder' => 'Иванов',
-            ],
-            'options'    => [
-                'label'            => 'Фамилия',
-                'label_attributes' => [
-                    'class' => 'form-label',
-                ],
-            ],
-        ]);
-
-        $this->add([
-            'name'       => 'name',
-            'type'       => Element\Text::class,
-            'attributes' => [
-                'class'       => 'form-control',
-                'placeholder' => 'Иван',
-            ],
-            'options'    => [
-                'label'            => 'Имя',
-                'label_attributes' => [
-                    'class' => 'form-label',
-                ],
-            ],
-        ]);
-
-        $this->add([
-            'name'       => 'patronymic',
-            'type'       => Element\Text::class,
-            'attributes' => [
-                'class'       => 'form-control',
-                'placeholder' => 'Иванович',
-            ],
-            'options'    => [
-                'label'            => 'Отчество',
-                'label_attributes' => [
-                    'class' => 'form-label',
-                ],
-            ],
-        ]);
-
-        $this->add([
-            'name'       => 'gender',
-            'type'       => Element\Select::class,
-            'attributes' => [
-                'class'    => 'form-select',
-                'required' => 'required',
-            ],
-            'options'    => [
-                'label'            => 'Пол',
-                'label_attributes' => [
-                    'class' => 'form-label',
-                ],
-                'options'          => GenderOptions::getOptions(),
-            ],
-        ]);
-
-        $this->add([
-            'name'       => 'birthday',
-            'type'       => Element\Date::class,
-            'attributes' => [
-                'class' => 'form-control',
-            ],
-            'options'    => [
-                'label'            => 'Дата рождения',
-                'label_attributes' => [
-                    'class' => 'form-label',
-                ],
-            ],
-        ]);
-
-        $this->add([
-            'name'       => 'skype',
-            'type'       => Element\Text::class,
-            'attributes' => [
-                'class'       => 'form-control',
-                'placeholder' => 'ivanivanov',
-            ],
-            'options'    => [
-                'label'            => 'Skype',
-                'label_attributes' => [
-                    'class' => 'form-label',
-                ],
-            ],
-        ]);
-
-        $this->add([
-            'name'       => 'submit-button',
-            'type'       => Element\Button::class,
-            'attributes' => [
-                'type'  => 'submit',
+                'value' => 'Сохранить изменения',
                 'class' => 'btn btn-outline-success w-100',
             ],
-            'options'    => [
-                'label' => 'Сохранить изменения',
-            ],
-        ], [
-            'priority' => -10 ** 9,
         ]);
 
         FieldsetMapper::setMapping($this, [
-            'photo'         => 'col-12',
-            'surname'       => 'col-12 col-lg-4',
-            'name'          => 'col-12 col-sm-6 col-lg-4',
-            'patronymic'    => 'col-12 col-sm-6 col-lg-4',
-            'gender'        => 'col-12 col-sm-6 col-lg-4',
-            'birthday'      => 'col-12 col-sm-6 col-lg-4',
-            'skype'         => 'col-12 col-lg-4',
-            'submit-button' => 'col-12',
+            'submit' => 'col-12',
         ]);
     }
 }
