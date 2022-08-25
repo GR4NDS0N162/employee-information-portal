f904fa3794813a54d11ea5354faecb73ca5b8420
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-25 18:57:14 +0300

    OGGYKIDS-203159: Remove the setMapping method

--- module/Application/src/Helper/FieldsetMapper.php
+++ module/Application/src/Helper/FieldsetMapper.php
@@ -2,18 +2,7 @@
 
 namespace Application\Helper;
 
-use Laminas\Form\FieldsetInterface;
-
 class FieldsetMapper
 {
     public const KEY = 'delimiter_class';
-
-    public static function setMapping(FieldsetInterface $fieldset, array $map)
-    {
-        foreach ($map as $name => $class) {
-            if ($fieldset->has($name)) {
-                $fieldset->get($name)->setAttribute(self::KEY, $class);
-            }
-        }
-    }
 }
