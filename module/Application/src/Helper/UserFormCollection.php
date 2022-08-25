7182a1f8ddcceed7c1ba9965139d54b5ac0a5683
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-25 15:43:48 +0300

    OGGYKIDS-203159: Initialize the view helper

--- module/Application/src/Helper/UserFormCollection.php
+++ module/Application/src/Helper/UserFormCollection.php
@@ -17,6 +17,15 @@ class UserFormCollection extends FormCollection
         Doctype::XHTML5 => true,
     ];
 
+    public function __construct()
+    {
+        $this->setDefaultElementHelper('userFormRow');
+
+        $this->setWrapper('%2$s<div%4$s>%3$s%1$s</div>');
+        $this->setLabelWrapper('<div class="form-label">%s</div>');
+        $this->setTemplateWrapper('<span class="d-none" data-template="%s"></span>');
+    }
+
     public function render(ElementInterface $element): string
     {
         $renderer = $this->getView();
