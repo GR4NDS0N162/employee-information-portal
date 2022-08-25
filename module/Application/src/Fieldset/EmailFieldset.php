72a3547d35f72d4c53aba2d4c8c9d8ec407d41a0
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-25 11:08:02 +0300

    OGGYKIDS-203159: Add fields to fieldsets

--- module/Application/src/Fieldset/EmailFieldset.php
+++ module/Application/src/Fieldset/EmailFieldset.php
@@ -2,11 +2,22 @@
 
 namespace Application\Fieldset;
 
+use Laminas\Form\Element\Email;
 use Laminas\Form\Fieldset;
 
 class EmailFieldset extends Fieldset
 {
     public function init()
     {
+        $this->add([
+            'name'       => 'address',
+            'type'       => Email::class,
+            'attributes' => [
+                'class'       => 'form-control',
+                'placeholder' => 'name@example.com',
+                'required'    => 'required',
+                'pattern'     => '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$',
+            ],
+        ]);
     }
 }
--- module/Application/src/Fieldset/PhoneFieldset.php
+++ module/Application/src/Fieldset/PhoneFieldset.php
@@ -2,11 +2,22 @@
 
 namespace Application\Fieldset;
 
+use Laminas\Form\Element\Tel;
 use Laminas\Form\Fieldset;
 
 class PhoneFieldset extends Fieldset
 {
     public function init()
     {
+        $this->add([
+            'name'       => 'number',
+            'type'       => Tel::class,
+            'attributes' => [
+                'class'       => 'form-control',
+                'placeholder' => '+7xxxxxxxxxx',
+                'required'    => 'required',
+                'pattern'     => '^\+7[0-9]{10}$',
+            ],
+        ]);
     }
 }
