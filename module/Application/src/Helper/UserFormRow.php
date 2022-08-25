0d108b3303b6ea257d7037a7a44a1dcf257133a6
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-25 18:44:18 +0300

    OGGYKIDS-203159: Create and use user-partial.phtml

--- module/Application/src/Helper/UserFormRow.php
+++ module/Application/src/Helper/UserFormRow.php
@@ -6,4 +6,8 @@ use Laminas\Form\View\Helper\FormRow;
 
 class UserFormRow extends FormRow
 {
+    public function __construct()
+    {
+        $this->setPartial('partial/user-partial.phtml');
+    }
 }
