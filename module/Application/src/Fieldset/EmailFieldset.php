742d5cc5881f1588a0c090807a6d0a879626825d
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-25 11:00:59 +0300

    OGGYKIDS-203159: Clear init methods

--- module/Application/src/Fieldset/EmailFieldset.php
+++ module/Application/src/Fieldset/EmailFieldset.php
@@ -2,55 +2,11 @@
 
 namespace Application\Fieldset;
 
-use Application\Form\FieldsetMapper;
-use Laminas\Form\Element\Button;
-use Laminas\Form\Element\Collection;
-use Laminas\Form\Element\Email;
 use Laminas\Form\Fieldset;
 
 class EmailFieldset extends Fieldset
 {
     public function init()
     {
-        $this->add([
-            'name'       => 'add',
-            'type'       => Button::class,
-            'attributes' => [
-                'type'  => 'button',
-                'class' => 'btn btn-outline-primary w-100',
-            ],
-            'options'    => [
-                'label' => 'Добавить e-mail',
-            ],
-        ]);
-
-        $this->add([
-            'name'       => 'list',
-            'type'       => Collection::class,
-            'attributes' => [
-                'class' => 'row gy-3',
-            ],
-            'options'    => [
-                'target_element'         => [
-                    'type'       => Email::class,
-                    'attributes' => [
-                        'class'       => 'form-control',
-                        'placeholder' => 'name@example.com',
-                        'required'    => 'required',
-                        'pattern'     => '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$',
-                    ],
-                ],
-                'count'                  => 1,
-                'allow_add'              => true,
-                'allow_remove'           => true,
-                'should_create_template' => true,
-                'template_placeholder'   => '__index__',
-            ],
-        ]);
-
-        FieldsetMapper::setMapping($this, [
-            'add'  => 'col-12',
-            'list' => 'col-12',
-        ]);
     }
 }
--- module/Application/src/Fieldset/PhoneFieldset.php
+++ module/Application/src/Fieldset/PhoneFieldset.php
@@ -2,55 +2,11 @@
 
 namespace Application\Fieldset;
 
-use Application\Form\FieldsetMapper;
-use Laminas\Form\Element\Button;
-use Laminas\Form\Element\Collection;
-use Laminas\Form\Element\Tel;
 use Laminas\Form\Fieldset;
 
 class PhoneFieldset extends Fieldset
 {
     public function init()
     {
-        $this->add([
-            'name'       => 'add',
-            'type'       => Button::class,
-            'attributes' => [
-                'type'  => 'button',
-                'class' => 'btn btn-outline-primary w-100',
-            ],
-            'options'    => [
-                'label' => 'Добавить телефон',
-            ],
-        ]);
-
-        $this->add([
-            'name'       => 'list',
-            'type'       => Collection::class,
-            'attributes' => [
-                'class' => 'row gy-3',
-            ],
-            'options'    => [
-                'target_element'         => [
-                    'type'       => Tel::class,
-                    'attributes' => [
-                        'class'       => 'form-control',
-                        'placeholder' => '+7xxxxxxxxxx',
-                        'required'    => 'required',
-                        'pattern'     => '^\+7[0-9]{10}$',
-                    ],
-                ],
-                'count'                  => 0,
-                'allow_add'              => true,
-                'allow_remove'           => true,
-                'should_create_template' => true,
-                'template_placeholder'   => '__index__',
-            ],
-        ]);
-
-        FieldsetMapper::setMapping($this, [
-            'add'  => 'col-12',
-            'list' => 'col-12',
-        ]);
     }
 }
