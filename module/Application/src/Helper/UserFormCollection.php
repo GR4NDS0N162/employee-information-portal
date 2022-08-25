130ab89e14eb1a6a7b464ef2863c2cc60d92e745
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-25 20:33:25 +0300

    OGGYKIDS-203159: Edit the logic of the UserFormCollection class

--- module/Application/src/Helper/UserFormCollection.php
+++ module/Application/src/Helper/UserFormCollection.php
@@ -2,32 +2,39 @@
 
 namespace Application\Helper;
 
+use Laminas\Form\Element\Collection;
 use Laminas\Form\ElementInterface;
 use Laminas\Form\View\Helper\FormCollection;
-use Laminas\View\Helper\Doctype;
 
 class UserFormCollection extends FormCollection
 {
-    /** @var array */
-    private $doctypesAllowedToHaveNameAttribute = [
-        Doctype::HTML5  => true,
-        Doctype::XHTML5 => true,
-    ];
-
     public function __construct()
     {
         $this->setDefaultElementHelper('userFormRow');
 
         $this->setWrapper('%2$s<div%4$s>%3$s%1$s</div>');
-        $this->setLabelWrapper('<div class="form-label">%s</div>');
-        $this->setTemplateWrapper('<span class="d-none" data-template="%s"></span>');
+        $this->setLabelWrapper('<div class="form-label order-first">%s</div>');
+        $this->setTemplateWrapper('<span class="d-none" data-template="%s"></span>' .
+            '<div class="notification">Список пуст.</div>');
     }
 
     public function render(ElementInterface $element): string
     {
-        return sprintf('<div class="%2$s">%1$s</div>',
-            parent::render($element),
-            $element->getAttribute(FieldsetMapper::KEY),
-        );
+        $format = '%1$s';
+        $values = [parent::render($element)];
+
+        if ($element instanceof Collection) {
+            $values[] = $element->getAttribute(FieldsetMapper::KEY);
+            $format = sprintf('<div class="card d-flex p-3">%1$s%2$s</div>', $format,
+                '<button type="button" name="add" class="btn btn-outline-primary order-first mb-3" value="">Добавить в список</button>');
+        }
+
+        if ($element->hasAttribute(FieldsetMapper::KEY)) {
+            $values[] = $element->getAttribute(FieldsetMapper::KEY);
+            $format = sprintf('<div class="%2$s">%1$s</div>', $format,
+                '%' . count($values) . '$s');
+        }
+
+        return sprintf($format, ...$values);
     }
 }
