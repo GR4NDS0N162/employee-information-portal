8c12c7257077e6947dfd0194b338daf6c141d46c
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-25 14:06:40 +0300

    OGGYKIDS-203159: Create and register a UserFormRow helper

--- module/Application/config/view_helpers.php
+++ module/Application/config/view_helpers.php
@@ -8,9 +8,11 @@ return [
     'aliases'   => [
         'userFormHelper'       => Helper\UserFormHelper::class,
         'userCollectionHelper' => Helper\UserCollectionHelper::class,
+        'userFormRow'          => Helper\UserFormRow::class,
     ],
     'factories' => [
         Helper\UserFormHelper::class       => InvokableFactory::class,
         Helper\UserCollectionHelper::class => InvokableFactory::class,
+        Helper\UserFormRow::class          => InvokableFactory::class,
     ],
 ];
---
+++ module/Application/src/Helper/UserFormRow.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Application\Helper;
+
+use Laminas\Form\View\Helper\FormRow;
+
+class UserFormRow extends FormRow
+{
+}
