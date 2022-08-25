92307c63f55d0bca61232885b3633e8835a2bd62
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-25 19:26:47 +0300

    OGGYKIDS-203159: Add the setAttributes method

--- module/Application/src/Helper/FieldsetMapper.php
+++ module/Application/src/Helper/FieldsetMapper.php
@@ -2,7 +2,44 @@
 
 namespace Application\Helper;
 
+use Laminas\Form\Element\Collection;
+use Laminas\Form\ElementInterface;
+use Laminas\Form\FieldsetInterface;
+
 class FieldsetMapper
 {
     public const KEY = 'delimiter_class';
+
+    /**
+     * @param ElementInterface $element
+     * @param string|array     $config
+     *
+     * @return void
+     */
+    public static function setAttributes(ElementInterface $element, $config)
+    {
+        if (empty($config))
+            return;
+
+        if (is_string($config)) {
+            $element->setAttribute(self::KEY, $config);
+            return;
+        }
+
+        if (is_string($config['value']) and !empty($config['value']))
+            $element->setAttribute(self::KEY, $config['value']);
+
+        if (($element instanceof Collection) and !empty($config['target_element'])) {
+            self::setAttributes($element->getTargetElement(), $config['target_element']);
+            return;
+        }
+
+        if (($element instanceof FieldsetInterface) and is_array($config['children'])) {
+            foreach ($config['children'] as $childName => $childConfig) {
+                if ($element->has($childName)) {
+                    self::setAttributes($element->get($childName), $childConfig);
+                }
+            }
+        }
+    }
 }
