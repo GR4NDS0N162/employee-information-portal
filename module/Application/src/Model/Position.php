c10c5741834d6e3a7e00e647f8305dd956cfabdf
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-26 17:48:08 +0300

    OGGYKIDS-203159: Create a Position class

---
+++ module/Application/src/Model/Position.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Application\Model;
+
+class Position
+{
+    private $id;
+    private $name;
+
+    public function __construct($name, $id = null)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
