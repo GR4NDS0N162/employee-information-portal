b702b7c935e1b9ef36be7eb0771d23c427f4a9e9
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-26 18:20:14 +0300

    OGGYKIDS-203159: Create an admin user editing form

--- module/Application/config/form_elements.php
+++ module/Application/config/form_elements.php
@@ -8,6 +8,7 @@ return [
     'aliases'   => [
     ],
     'factories' => [
+        Form\UserForm::class        => InvokableFactory::class,
         Form\ProfileForm::class     => InvokableFactory::class,
         Form\ViewProfileForm::class => InvokableFactory::class,
         Form\PositionForm::class    => InvokableFactory::class,
