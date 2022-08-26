3b6ac636aa302ec41666e264c7e831dd8471be89
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-27 01:10:57 +0300

    OGGYKIDS-203159: Move the array of the delete button from the collection to a separate file

--- module/Application/src/Fieldset/EmailFieldset.php
+++ module/Application/src/Fieldset/EmailFieldset.php
@@ -25,18 +25,6 @@ class EmailFieldset extends Fieldset
             ],
         ]);
 
-        $this->add([
-            'name'       => 'delete',
-            'type'       => Element\Button::class,
-            'attributes' => [
-                'class' => 'btn btn-outline-danger',
-            ],
-            'options'    => [
-                'label'         => '<svg width="24" height="24" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px}</style></defs><title></title><g id="cross"><line class="cls-1" x1="7" x2="25" y1="7" y2="25"></line><line class="cls-1" x1="7" x2="25" y1="25" y2="7"></line></g></svg>',
-                'label_options' => [
-                    'disable_html_escape' => true,
-                ],
-            ],
-        ]);
+        $this->add(include __DIR__ . '/../ElementArray/DeleteButton.php');
     }
 }
