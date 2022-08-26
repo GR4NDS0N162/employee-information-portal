3ce65def43a6bf6f4d74d4993c1577362e049546
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-26 20:26:39 +0300

    OGGYKIDS-203159: Create and use ViewForm instead UserForm

--- module/Application/config/view_helpers.php
+++ module/Application/config/view_helpers.php
@@ -7,11 +7,13 @@ use Laminas\ServiceManager\Factory\InvokableFactory;
 return [
     'aliases'   => [
         'userForm'           => Helper\UserForm::class,
+        'viewForm'           => Helper\ViewForm::class,
         'userFormCollection' => Helper\UserFormCollection::class,
         'userFormRow'        => Helper\UserFormRow::class,
     ],
     'factories' => [
         Helper\UserForm::class           => InvokableFactory::class,
+        Helper\ViewForm::class           => InvokableFactory::class,
         Helper\UserFormCollection::class => InvokableFactory::class,
         Helper\UserFormRow::class        => InvokableFactory::class,
     ],
---
+++ module/Application/src/Helper/ViewForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Application\Helper;
+
+use Laminas\Form\FormInterface;
+
+class ViewForm extends UserForm
+{
+    public function render(FormInterface $form): string
+    {
+        return preg_replace(
+            [
+                '/<button.*button>/',
+                '/<input.*type="(submit|button)".*>/',
+            ],
+            '', parent::render($form));
+    }
+}
