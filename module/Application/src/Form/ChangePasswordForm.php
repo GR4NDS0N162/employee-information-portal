4fd56bc725a8c251105950d1d931b097201db305
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-26 19:52:12 +0300

    OGGYKIDS-203159: Add the missing fields to the form

--- module/Application/src/Form/ChangePasswordForm.php
+++ module/Application/src/Form/ChangePasswordForm.php
@@ -4,14 +4,18 @@ namespace Application\Form;
 
 use Application\Helper\FieldsetMapper;
 use Laminas\Form\Element;
+use Laminas\Form\Form;
 
-class ChangePasswordForm extends SetPasswordForm
+class ChangePasswordForm extends Form
 {
     public const DEFAULT_NAME = 'change-password-form';
 
     public function __construct($name = self::DEFAULT_NAME)
     {
         parent::__construct($name);
+
+        $this->setAttribute('class', 'row gy-3 needs-validation');
+        $this->setAttribute('novalidate', '');
 
         $this->add([
             'name'       => 'current-password',
@@ -28,11 +32,56 @@ class ChangePasswordForm extends SetPasswordForm
                     'class' => 'form-label',
                 ],
             ],
-        ], [
-            'priority' => 100,
         ]);
 
-        $this->get('submit-button')->setLabel('Изменить пароль');
+        $this->add([
+            'name'       => 'new-password',
+            'type'       => Element\Password::class,
+            'attributes' => [
+                'class'        => 'form-control',
+                'placeholder'  => 'qwerty123',
+                'required'     => 'required',
+                'autocomplete' => 'new-password',
+                'minlength'    => 8,
+                'maxlength'    => 32,
+                'pattern'      => '^(?=.*?[а-яa-z])(?=.*?[А-ЯA-Z])(?=.*?[0-9])(?=.*?[!"#\$%&\'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~])[а-яa-zА-ЯA-Z0-9!"#\$%&\'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~]*$',
+            ],
+            'options'    => [
+                'label'            => 'Новый пароль',
+                'label_attributes' => [
+                    'class' => 'form-label',
+                ],
+            ],
+        ]);
+
+        $this->add([
+            'name'       => 'password-check',
+            'type'       => Element\Password::class,
+            'attributes' => [
+                'class'       => 'form-control',
+                'placeholder' => 'qwerty123',
+                'required'    => 'required',
+                'pattern'     => '',
+            ],
+            'options'    => [
+                'label'            => 'Подтверждение пароля',
+                'label_attributes' => [
+                    'class' => 'form-label',
+                ],
+            ],
+        ]);
+
+        $this->add([
+            'name'       => 'submit-button',
+            'type'       => Element\Button::class,
+            'attributes' => [
+                'type'  => 'submit',
+                'class' => 'btn btn-outline-success w-100',
+            ],
+            'options'    => [
+                'label' => 'Изменить пароль',
+            ],
+        ]);
 
         FieldsetMapper::setAttributes($this, [
             'children' => [
