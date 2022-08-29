19a326225f8650bbdbc349bbf5971c4a784a4871
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-29 22:12:22 +0300

    OGGYKIDS-204246: Delete all InvokableFactory assignments

--- module/Application/config/form_elements.php
+++ module/Application/config/form_elements.php
@@ -2,28 +2,12 @@
 
 namespace Application;
 
-use Laminas\ServiceManager\Factory\InvokableFactory;
-
 return [
     'aliases'   => [
     ],
     'factories' => [
         Fieldset\UserFieldset::class => Factory\Fieldset\UserFieldsetFactory::class,
 
-        Form\Admin\UserForm::class        => InvokableFactory::class,
-        Form\Admin\PositionForm::class    => InvokableFactory::class,
-        Form\Admin\AdminFilterForm::class => InvokableFactory::class,
-
-        Form\Login\LoginForm::class   => InvokableFactory::class,
-        Form\Login\SignUpForm::class  => Factory\Form\SignUpFormFactory::class,
-        Form\Login\RecoverForm::class => InvokableFactory::class,
-
-        Form\Messenger\NewMessageForm::class   => InvokableFactory::class,
-        Form\Messenger\DialogFilterForm::class => InvokableFactory::class,
-
-        Form\User\ProfileForm::class        => InvokableFactory::class,
-        Form\User\ChangePasswordForm::class => InvokableFactory::class,
-        Form\User\ViewProfileForm::class    => InvokableFactory::class,
-        Form\User\UserFilterForm::class     => InvokableFactory::class,
+        Form\Login\SignUpForm::class => Factory\Form\SignUpFormFactory::class,
     ],
 ];
