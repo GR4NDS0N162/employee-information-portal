20e94becaa453677361a610e336ca8a4046fb083
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-30 01:58:02 +0300

    OGGYKIDS-204246: Implement the insertUser() method

--- module/Application/src/Model/Command/UserCommand.php
+++ module/Application/src/Model/Command/UserCommand.php
@@ -7,7 +7,11 @@ use Application\Model\Entity\Email;
 use Application\Model\Entity\User;
 use Application\Model\UserCommandInterface;
 use Application\Model\UserRepositoryInterface;
+use InvalidArgumentException;
 use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Adapter\Driver\ResultInterface;
+use Laminas\Db\Sql\Sql;
+use RuntimeException;
 
 class UserCommand implements UserCommandInterface
 {
@@ -36,9 +40,59 @@ class UserCommand implements UserCommandInterface
         $this->userRepository = $userRepository;
     }
 
+    /**
+     * @inheritdoc
+     * @throws RuntimeException
+     * @throws InvalidArgumentException
+     */
     public function insertUser(User $user, Email $email)
     {
-        // TODO: Implement insertUser() method.
+        try {
+            $foundEmail = $this->emailRepository->findEmail($email->getAddress());
+        } catch (InvalidArgumentException $ex) {
+            $sql = new Sql($this->db);
+
+            $insert = $sql->insert('user');
+            $insert->values([
+                'password'    => $user->getPassword(),
+                'position_id' => $user->getPositionId(),
+            ]);
+
+            $statement = $sql->prepareStatementForSqlObject($insert);
+            $result = $statement->execute();
+
+            if (!$result instanceof ResultInterface) {
+                throw new RuntimeException(
+                    'Database error occurred during user insert operation.'
+                );
+            }
+
+            $id = $result->getGeneratedValue();
+
+            $insert = $sql->insert('email');
+            $insert->values([
+                'address' => $email->getAddress(),
+                'user_id' => $id,
+            ]);
+
+            $statement = $sql->prepareStatementForSqlObject($insert);
+            $result = $statement->execute();
+
+            if (!$result instanceof ResultInterface) {
+                throw new RuntimeException(
+                    'Database error occurred during email insert operation'
+                );
+            }
+        }
+
+        if (!empty($foundEmail)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Email with address %s already exists in the system.',
+                    $email->getAddress()
+                )
+            );
+        }
     }
 
     public function setTempPassword(Email $email)
