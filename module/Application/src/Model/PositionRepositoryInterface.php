351cf3b21a9103436fce5c24902b0ef15477d6e0
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-29 21:52:52 +0300

    OGGYKIDS-204246: Specify the returned type

--- module/Application/src/Model/PositionRepositoryInterface.php
+++ module/Application/src/Model/PositionRepositoryInterface.php
@@ -3,11 +3,12 @@
 namespace Application\Model;
 
 use Application\Model\Entity\Position;
+use Laminas\Db\ResultSet\HydratingResultSet;
 
 interface PositionRepositoryInterface
 {
     /**
-     * @return Position[]
+     * @return Position[]|HydratingResultSet
      */
-    public function findAllPositions(): array;
+    public function findAllPositions();
 }
