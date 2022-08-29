1d33398141de1f187ce59113f0c9d09c77c801a2
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-29 20:22:50 +0300

    OGGYKIDS-204246: Create the Extractor class

---
+++ module/Application/src/Model/Repository/Extractor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Model\Repository;
+
+use Laminas\Db\Adapter\Driver\ResultInterface;
+use Laminas\Db\ResultSet\HydratingResultSet;
+use Laminas\Db\Sql\Select;
+use Laminas\Db\Sql\Sql;
+use Laminas\Hydrator\HydratorInterface;
+
+class Extractor
+{
+    public static function extractArray(
+        Sql               $sql,
+        Select            $select,
+        HydratorInterface $hydrator,
+                          $prototype
+    ) {
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $result = $statement->execute();
+
+        if (!$result instanceof ResultInterface || !$result->isQueryResult()) {
+            return [];
+        }
+
+        $resultSet = new HydratingResultSet($hydrator, $prototype);
+        $resultSet->initialize($result);
+        return $resultSet;
+    }
+}
