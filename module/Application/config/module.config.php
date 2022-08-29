81ceace2b179e66511bd9b50f59635bd8276b3d7
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-29 18:30:32 +0300

    OGGYKIDS-204246: Add an empty ServiceManager configuration

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -3,10 +3,11 @@
 namespace Application;
 
 return [
-    'form_elements' => include __DIR__ . '/form_elements.php',
-    'router'        => include __DIR__ . '/router.php',
-    'navigation'    => include __DIR__ . '/navigation.php',
-    'controllers'   => include __DIR__ . '/controllers.php',
-    'view_manager'  => include __DIR__ . '/view_manager.php',
-    'view_helpers'  => include __DIR__ . '/view_helpers.php',
+    'service_manager' => include __DIR__ . '/service_manager.php',
+    'form_elements'   => include __DIR__ . '/form_elements.php',
+    'router'          => include __DIR__ . '/router.php',
+    'navigation'      => include __DIR__ . '/navigation.php',
+    'controllers'     => include __DIR__ . '/controllers.php',
+    'view_manager'    => include __DIR__ . '/view_manager.php',
+    'view_helpers'    => include __DIR__ . '/view_helpers.php',
 ];
