ad75cacabf17014eb04fc33abbc5635031d185ac
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-29 18:07:16 +0300

    OGGYKIDS-204246: Add the function to get emails by user id

--- module/Application/src/Model/EmailRepositoryInterface.php
+++ module/Application/src/Model/EmailRepositoryInterface.php
@@ -4,4 +4,10 @@ namespace Application\Model;
 
 interface EmailRepositoryInterface
 {
+    /**
+     * @param integer $userId
+     *
+     * @return Email[]
+     */
+    public function findEmailsOfUser(int $userId): array;
 }
