689924054e34b75a1055c57fc096bc117ad04f6e
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-29 21:34:59 +0300

    OGGYKIDS-204246: Delete the hardcoded data

--- module/Application/src/Model/Options/PositionOptions.php
+++ module/Application/src/Model/Options/PositionOptions.php
@@ -6,17 +6,6 @@ use Application\Model\PositionRepositoryInterface;
 
 class PositionOptions
 {
-    private const positions = [
-        '1' => 'Уборщик',
-        '2' => 'Фасовщик',
-        '3' => 'Менеджер',
-        '4' => 'Швейцар',
-        '5' => 'Шеф',
-        '6' => 'Экономист',
-        '7' => 'Электрик',
-        '8' => 'Юрист',
-    ];
-
     /**
      * @var PositionRepositoryInterface
      */
