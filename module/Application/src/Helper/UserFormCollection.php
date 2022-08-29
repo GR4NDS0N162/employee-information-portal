59633a21a09bf6c8069f47424baf0e6f05137d55
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-29 23:41:37 +0300

    OGGYKIDS-204246: Format the document

--- module/Application/src/Helper/UserFormCollection.php
+++ module/Application/src/Helper/UserFormCollection.php
@@ -14,8 +14,10 @@ class UserFormCollection extends FormCollection
 
         $this->setWrapper('%2$s<div%4$s>%3$s%1$s</div>');
         $this->setLabelWrapper('<div class="form-label order-first">%s</div>');
-        $this->setTemplateWrapper('<span class="d-none" data-template="%s"></span>' .
-            '<div class="notification">Список пуст.</div>');
+        $this->setTemplateWrapper(
+            '<span class="d-none" data-template="%s"></span>' .
+            '<div class="notification">Список пуст.</div>'
+        );
     }
 
     public function render(ElementInterface $element): string
@@ -25,14 +27,20 @@ class UserFormCollection extends FormCollection
 
         if ($element instanceof Collection) {
             $values[] = $element->getAttribute(FieldsetMapper::KEY);
-            $format = sprintf('<div class="card d-flex p-3">%1$s%2$s</div>', $format,
-                '<button type="button" name="add" class="btn btn-outline-primary order-first mb-3" value="">Добавить в список</button>');
+            $format = sprintf(
+                '<div class="card d-flex p-3">%1$s%2$s</div>',
+                $format,
+                '<button type="button" name="add" class="btn btn-outline-primary order-first mb-3" value="">Добавить в список</button>'
+            );
         }
 
         if ($element->hasAttribute(FieldsetMapper::KEY)) {
             $values[] = $element->getAttribute(FieldsetMapper::KEY);
-            $format = sprintf('<div class="%2$s">%1$s</div>', $format,
-                '%' . count($values) . '$s');
+            $format = sprintf(
+                '<div class="%2$s">%1$s</div>',
+                $format,
+                '%' . count($values) . '$s'
+            );
         }
 
         return sprintf($format, ...$values);
