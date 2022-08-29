18e3469d1bc7b9077b087c2c2528a9c353a8afb1
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-29 18:27:20 +0300

    OGGYKIDS-204246: Create a PositionRepository class

---
+++ module/Application/src/Model/Repository/PositionRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Model\Repository;
+
+use Application\Model\PositionRepositoryInterface;
+
+class PositionRepository implements PositionRepositoryInterface
+{
+    public function findAllPositions(): array
+    {
+        // TODO: Implement findAllPositions() method.
+    }
+}
