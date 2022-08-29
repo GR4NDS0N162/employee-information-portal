6da79a77d8d4f3c6ed7d7e8ff21c972bf99922a8
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-30 01:37:45 +0300

    OGGYKIDS-204246: Implement the findEmail() method

--- module/Application/src/Model/Repository/EmailRepository.php
+++ module/Application/src/Model/Repository/EmailRepository.php
@@ -4,8 +4,12 @@ namespace Application\Model\Repository;
 
 use Application\Model\EmailRepositoryInterface;
 use Application\Model\Entity\Email;
+use Application\Model\Extractor;
+use InvalidArgumentException;
 use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Sql\Sql;
 use Laminas\Hydrator\HydratorInterface;
+use RuntimeException;
 
 class EmailRepository implements EmailRepositoryInterface
 {
@@ -39,8 +43,45 @@ class EmailRepository implements EmailRepositoryInterface
         // TODO: Implement findEmailsOfUser() method.
     }
 
+    /**
+     * @inheritdoc
+     * @throws RuntimeException
+     * @throws InvalidArgumentException
+     */
     public function findEmail(string $address): Email
     {
-        // TODO: Implement findEmail() method.
+        $sql = new Sql($this->db);
+        $select = $sql->select('email');
+        $select->columns([
+            'address',
+            'userId' => 'user_id',
+        ]);
+        $select->where(['address = ?' => $address]);
+
+        $email = Extractor::extractValue(
+            $sql,
+            $select,
+            $this->hydrator,
+            $this->emailPrototype,
+            sprintf(
+                'Failed retrieving email with address "%s"; unknown database error.',
+                $address
+            ),
+            sprintf(
+                'Email with address "%s" not found.',
+                $address
+            ),
+        );
+
+        if ($email instanceof Email) {
+            return $email;
+        }
+
+        throw new RuntimeException(
+            sprintf(
+                'Failed retrieving email with address "%s"; unknown repository error.',
+                $address
+            )
+        );
     }
 }
