16f14a0222fd5599d8bc97627c7ba2777fba5f84
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-29 19:46:31 +0300

    OGGYKIDS-204246: Assign an InvokableFactory to the forms

--- module/Application/config/form_elements.php
+++ module/Application/config/form_elements.php
@@ -8,9 +8,20 @@ return [
     'aliases'   => [
     ],
     'factories' => [
-        Form\Admin\UserForm::class       => InvokableFactory::class,
-        Form\User\ProfileForm::class     => InvokableFactory::class,
-        Form\User\ViewProfileForm::class => InvokableFactory::class,
-        Form\Admin\PositionForm::class   => InvokableFactory::class,
+        Form\Admin\UserForm::class        => InvokableFactory::class,
+        Form\Admin\PositionForm::class    => InvokableFactory::class,
+        Form\Admin\AdminFilterForm::class => InvokableFactory::class,
+
+        Form\Login\LoginForm::class   => InvokableFactory::class,
+        Form\Login\SignUpForm::class  => InvokableFactory::class,
+        Form\Login\RecoverForm::class => InvokableFactory::class,
+
+        Form\Messenger\NewMessageForm::class   => InvokableFactory::class,
+        Form\Messenger\DialogFilterForm::class => InvokableFactory::class,
+
+        Form\User\ProfileForm::class        => InvokableFactory::class,
+        Form\User\ChangePasswordForm::class => InvokableFactory::class,
+        Form\User\ViewProfileForm::class    => InvokableFactory::class,
+        Form\User\UserFilterForm::class     => InvokableFactory::class,
     ],
 ];
