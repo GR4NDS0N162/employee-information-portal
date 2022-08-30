571aaacf64b788ec780eac853ab3158d2fddbd7b
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-30 14:58:59 +0300

    OGGYKIDS-204246: Add field

--- module/Application/src/Model/Entity/Phone.php
+++ module/Application/src/Model/Entity/Phone.php
@@ -5,14 +5,23 @@ namespace Application\Model\Entity;
 class Phone
 {
     private string $number;
+    private ?int $userId;
 
-    public function __construct(string $number)
-    {
+    public function __construct(
+        string $number,
+        ?int   $userId = null
+    ) {
         $this->number = $number;
+        $this->userId = $userId;
     }
 
     public function getNumber(): string
     {
         return $this->number;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
     }
 }
