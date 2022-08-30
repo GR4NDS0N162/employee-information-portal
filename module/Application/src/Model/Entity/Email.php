eebf03fe79d6b860d330cd580c860740b46834ca
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-30 12:43:31 +0300

    OGGYKIDS-204246: Add field $userId

--- module/Application/src/Model/Entity/Email.php
+++ module/Application/src/Model/Entity/Email.php
@@ -5,14 +5,23 @@ namespace Application\Model\Entity;
 class Email
 {
     private string $address;
+    private ?int $userId;
 
-    public function __construct(string $address)
-    {
+    public function __construct(
+        string $address,
+        ?int   $userId = null
+    ) {
         $this->address = $address;
+        $this->userId = $userId;
     }
 
     public function getAddress(): string
     {
         return $this->address;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
     }
 }
