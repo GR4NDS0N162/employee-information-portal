5fc3b92f326f5a3db20ea5e79c7142b511d18758
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-30 16:46:50 +0300

    OGGYKIDS-204246: Implement dependencies in StatusRepository

--- module/Application/src/Factory/Repository/StatusRepositoryFactory.php
+++ module/Application/src/Factory/Repository/StatusRepositoryFactory.php
@@ -2,14 +2,21 @@
 
 namespace Application\Factory\Repository;
 
+use Application\Model\Entity\Status;
 use Application\Model\Repository\StatusRepository;
 use Interop\Container\ContainerInterface;
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Hydrator\ReflectionHydrator;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class StatusRepositoryFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
-        return new StatusRepository();
+        return new StatusRepository(
+            $container->get(AdapterInterface::class),
+            new ReflectionHydrator(),
+            new Status(),
+        );
     }
 }
--- module/Application/src/Model/Repository/StatusRepository.php
+++ module/Application/src/Model/Repository/StatusRepository.php
@@ -2,8 +2,35 @@
 
 namespace Application\Model\Repository;
 
+use Application\Model\Entity\Status;
 use Application\Model\StatusRepositoryInterface;
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Hydrator\HydratorInterface;
 
 class StatusRepository implements StatusRepositoryInterface
 {
+    /**
+     * @var AdapterInterface
+     */
+    private $db;
+
+    /**
+     * @var HydratorInterface
+     */
+    private $hydrator;
+
+    /**
+     * @var Status
+     */
+    private $statusPrototype;
+
+    public function __construct(
+        AdapterInterface  $db,
+        HydratorInterface $hydrator,
+        Status            $statusPrototype
+    ) {
+        $this->db = $db;
+        $this->hydrator = $hydrator;
+        $this->statusPrototype = $statusPrototype;
+    }
 }
