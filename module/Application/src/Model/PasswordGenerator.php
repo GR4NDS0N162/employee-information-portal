1acb81bf48c88e2e9da01ad192035dbcdb1ca2f4
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-30 12:29:20 +0300

    OGGYKIDS-204246: Create a PasswordGenerator class

---
+++ module/Application/src/Model/PasswordGenerator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Application\Model;
+
+use Exception;
+
+class PasswordGenerator
+{
+    /**
+     * @throws Exception
+     */
+    public static function generate(int $length = 8): string
+    {
+        $chars = [
+            [
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            ],
+            [
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+            ],
+            ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+            [
+                '!', '"', '#', '$', '%', '&', '\'', '\\', ')', '*',
+                '+', ',', '-', '.', '/', ':', ';', '<', '=', '>',
+                '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'
+            ]
+        ];
+
+        $password = '';
+
+        foreach ($chars as $charset) {
+            $password .= $charset[random_int(0, count($charset) - 1)];
+        }
+
+        for ($i = 0; $i < $length - count($chars); $i++) {
+            $charset = $chars[random_int(0, count($chars) - 1)];
+            $password .= $charset[random_int(0, count($charset) - 1)];
+        }
+
+        return str_shuffle($password);
+    }
+}
