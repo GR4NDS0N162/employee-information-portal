46fc6db4d3716eb29bca2bbdaa6d834a0c22b569
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-31 22:50:06 +0300

    OGGYKIDS-204246: Implement dependencies on AdapterInterface and PositionRepositoryInterface

--- module/Application/src/Model/Command/PositionCommand.php
+++ module/Application/src/Model/Command/PositionCommand.php
@@ -2,6 +2,27 @@
 
 namespace Application\Model\Command;
 
+use Application\Model\Repository\PositionRepositoryInterface;
+use Laminas\Db\Adapter\AdapterInterface;
+
 class PositionCommand implements PositionCommandInterface
 {
+    /**
+     * @var AdapterInterface
+     */
+    private $db;
+    /**
+     * @var PositionRepositoryInterface
+     */
+    private $positionRepository;
+
+    /**
+     * @param AdapterInterface            $db
+     * @param PositionRepositoryInterface $positionRepository
+     */
+    public function __construct($db, $positionRepository)
+    {
+        $this->db = $db;
+        $this->positionRepository = $positionRepository;
+    }
 }
