a5bceee9da2906fb0e920410e8f6d93e2f1d51dd
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-31 22:38:18 +0300

    OGGYKIDS-204246: Create a PositionListFieldset class

---
+++ module/Application/src/Fieldset/PositionListFieldset.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Application\Fieldset;
+
+use Application\Model\Entity\PositionList;
+use Laminas\Form\Element\Collection;
+use Laminas\Form\Fieldset;
+use Laminas\Hydrator\ClassMethodsHydrator;
+
+class PositionListFieldset extends Fieldset
+{
+    public function init()
+    {
+        parent::init();
+
+        $this->setHydrator(new ClassMethodsHydrator(false, true));
+        $this->setObject(new PositionList());
+
+        $this->add([
+            'name'       => 'list',
+            'type'       => Collection::class,
+            'attributes' => [
+                'class' => 'row g-3',
+            ],
+            'options'    => [
+                'label'                  => 'Должности',
+                'count'                  => 0,
+                'allow_add'              => true,
+                'allow_remove'           => true,
+                'should_create_template' => true,
+                'template_placeholder'   => '__index__',
+                'target_element'         => [
+                    'type'       => PositionFieldset::class,
+                    'attributes' => [
+                        'class' => 'input-group',
+                    ],
+                ],
+            ],
+        ]);
+    }
+}
