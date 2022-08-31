9a7351b1f3ce43a2e511e0a1133885715858e73a
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-31 21:32:50 +0300

    OGGYKIDS-204246: Implement InputFilterAwareInterface

--- module/Application/src/Model/Entity/Email.php
+++ module/Application/src/Model/Entity/Email.php
@@ -2,7 +2,16 @@
 
 namespace Application\Model\Entity;
 
-class Email
+use DomainException;
+use Laminas\Filter\ToInt;
+use Laminas\InputFilter\InputFilter;
+use Laminas\InputFilter\InputFilterAwareInterface;
+use Laminas\InputFilter\InputFilterInterface;
+use Laminas\Validator\GreaterThan;
+use Laminas\Validator\Regex;
+use Laminas\Validator\StringLength;
+
+class Email implements InputFilterAwareInterface
 {
     /**
      * @var string
@@ -12,6 +21,10 @@ class Email
      * @var int|null
      */
     private $userId;
+    /**
+     * @var InputFilter
+     */
+    private $inputFilter;
 
     /**
      * @param string   $address
@@ -39,5 +52,62 @@ class Email
     public function getUserId()
     {
         return $this->userId;
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new DomainException(
+            sprintf(
+                '%s does not allow injection of an alternate input filter',
+                __CLASS__
+            )
+        );
+    }
+
+    public function getInputFilter()
+    {
+        if ($this->inputFilter) {
+            return $this->inputFilter;
+        }
+
+        $inputFilter = new InputFilter();
+
+        $inputFilter->add([
+            'name'       => 'userId',
+            'filters'    => [
+                ['name' => ToInt::class],
+            ],
+            'validators' => [
+                [
+                    'name'    => GreaterThan::class,
+                    'options' => [
+                        'min' => 0,
+                    ],
+                ],
+            ],
+        ]);
+
+        $inputFilter->add([
+            'name'       => 'address',
+            'required'   => true,
+            'validators' => [
+                [
+                    'name'    => StringLength::class,
+                    'options' => [
+                        'encoding' => 'UTF-8',
+                        'max'      => 320,
+                    ],
+                ],
+                [
+                    'name'    => Regex::class,
+                    'options' => [
+                        'pattern' => '/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/',
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->inputFilter = $inputFilter;
+        return $this->inputFilter;
     }
 }
