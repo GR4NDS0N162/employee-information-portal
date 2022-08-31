16bf2cb495ef3f8274fb104b603c41a5181a88ce
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-31 14:00:54 +0300

    OGGYKIDS-204246: Change the structure of StatusRepository

--- module/Application/src/Factory/Repository/StatusRepositoryFactory.php
+++ module/Application/src/Factory/Repository/StatusRepositoryFactory.php
@@ -3,7 +3,6 @@
 namespace Application\Factory\Repository;
 
 use Application\Model\Entity\Status;
-use Application\Model\Entity\UserStatus;
 use Application\Model\Repository\StatusRepository;
 use Interop\Container\ContainerInterface;
 use Laminas\Db\Adapter\AdapterInterface;
@@ -18,7 +17,6 @@ class StatusRepositoryFactory implements FactoryInterface
             $container->get(AdapterInterface::class),
             new ReflectionHydrator(),
             new Status(),
-            new UserStatus(),
         );
     }
 }
