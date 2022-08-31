aa689f58fae760c42f170d1478f8e5a7ebab9ce2
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-31 23:47:33 +0300

    OGGYKIDS-204246: Implement InputFilterAwareInterface

--- module/Application/src/Model/Entity/PositionList.php
+++ module/Application/src/Model/Entity/PositionList.php
@@ -2,12 +2,22 @@
 
 namespace Application\Model\Entity;
 
-class PositionList
+use DomainException;
+use Laminas\InputFilter\InputFilter;
+use Laminas\InputFilter\InputFilterAwareInterface;
+use Laminas\InputFilter\InputFilterInterface;
+use Laminas\Validator\IsCountable;
+
+class PositionList implements InputFilterAwareInterface
 {
     /**
      * @var Position[]
      */
     private $list;
+    /**
+     * @var InputFilter
+     */
+    private $inputFilter;
 
     /**
      * @param Position[] $list
@@ -23,5 +33,34 @@ class PositionList
     public function getList()
     {
         return $this->list;
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new DomainException(
+            sprintf(
+                '%s does not allow injection of an alternate input filter',
+                __CLASS__
+            )
+        );
+    }
+
+    public function getInputFilter()
+    {
+        if ($this->inputFilter) {
+            return $this->inputFilter;
+        }
+
+        $inputFilter = new InputFilter();
+
+        $inputFilter->add([
+            'name'       => 'list',
+            'validators' => [
+                ['name' => IsCountable::class,],
+            ],
+        ]);
+
+        $this->inputFilter = $inputFilter;
+        return $this->inputFilter;
     }
 }
