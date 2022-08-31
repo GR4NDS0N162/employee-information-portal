9ae346cd0361bf3485c06a671dd031a519f1d345
Kirill Vnukov <kirillgrandson@gmail.com>
2022-08-31 14:50:31 +0300

    OGGYKIDS-204246: Modify findUser() method

--- module/Application/src/Model/Repository/UserRepository.php
+++ module/Application/src/Model/Repository/UserRepository.php
@@ -76,79 +76,19 @@ class UserRepository implements UserRepositoryInterface
 
     public function findUser($identifier)
     {
-        $select = new Select('user');
-        $select->columns([
-            'id',
-            'password',
-            'tempPassword' => 'temp_password',
-            'tpCreatedAt'  => 'tp_created_at',
-            'positionId'   => 'position_id',
-            'surname',
-            'name',
-            'patronymic',
-            'gender',
-            'birthday',
-            'image',
-            'skype',
-        ]);
-
-        if (is_integer($identifier)) {
-            $select->where(['id = ?' => $identifier]);
-
-            $runtimeExceptionMessage = sprintf(
-                'Failed retrieving user with id "%d"; unknown database error',
-                $identifier
-            );
-            $invalidArgumentExceptionMessage = sprintf(
-                'User with id "%d" not found',
-                $identifier
-            );
+        if (is_int($identifier)) {
+            return $this->findUserById($identifier);
         } else if ($identifier instanceof Email) {
-            $select->where(['id = ?' => $identifier->getUserId()]);
-
-            $runtimeExceptionMessage = sprintf(
-                'Failed retrieving user with email address "%s"; unknown database error',
-                $identifier->getAddress()
-            );
-            $invalidArgumentExceptionMessage = sprintf(
-                'User with email address "%s" not found',
-                $identifier->getAddress()
-            );
+            return $this->findUserByEmail($identifier);
         } else {
             throw new InvalidArgumentException(
-                'An inappropriate type parameter was passed.'
+                sprintf(
+                    'The type of the $identifier parameter must be compatible with %1$s; %2$s used',
+                    implode(' or ', [gettype(1), Email::class]),
+                    (get_class($identifier) ?: gettype($identifier))
+                )
             );
         }
-
-        $user = Extracter::extractValue($select, $this->db, $this->hydrator, $this->prototype);
-
-        if (!$user instanceof User) {
-            if (is_integer($identifier)) {
-                throw new RuntimeException(
-                    sprintf(
-                        'Failed retrieving user with id "%d"; unknown repository error',
-                        $identifier
-                    )
-                );
-            } else {
-                throw new RuntimeException(
-                    sprintf(
-                        'Failed retrieving user with email address "%s"; unknown repository error',
-                        $identifier->getAddress()
-                    )
-                );
-            }
-        }
-
-        $emails = $this->emailRepository->findEmailsOfUser($user->getId());
-        $phones = $this->phoneRepository->findPhonesOfUser($user->getId());
-        $statusMap = $this->statusRepository->generateStatusMapOfUser($user->getId());
-
-        $user->setEmails($emails);
-        $user->setPhones($phones);
-        $user->setStatus($statusMap);
-
-        return $user;
     }
 
     public function findUserById($id)
