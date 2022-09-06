73f5409ec4b95e839e9b60c89d0f354ab5e8e9df
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-06 18:32:00 +0300

    OGGYKIDS-204246: Add private deletePhoneByNumber() and addPhone() methods

--- module/Application/src/Model/Command/PhoneCommand.php
+++ module/Application/src/Model/Command/PhoneCommand.php
@@ -2,7 +2,11 @@
 
 namespace Application\Model\Command;
 
+use Application\Model\Entity\Phone;
+use Application\Model\Executer;
 use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Sql\Delete;
+use Laminas\Db\Sql\Insert;
 
 class PhoneCommand implements PhoneCommandInterface
 {
@@ -22,5 +26,34 @@ class PhoneCommand implements PhoneCommandInterface
     public function updatePhones($oldList, $newList)
     {
         // TODO: Implement updatePhones() method.
+    }
+
+    /**
+     * @param string $number
+     *
+     * @return void
+     */
+    private function deletePhoneByNumber($number)
+    {
+        $delete = new Delete('phone');
+        $delete->where(['number = ?' => $number]);
+
+        Executer::executeSql($delete, $this->db);
+    }
+
+    /**
+     * @param Phone $phone
+     *
+     * @return void
+     */
+    private function addPhone($phone)
+    {
+        $insert = new Insert('phone');
+        $insert->values([
+            'user_id' => $phone->getUserId(),
+            'number'  => $phone->getNumber(),
+        ]);
+
+        Executer::executeSql($insert, $this->db);
     }
 }
