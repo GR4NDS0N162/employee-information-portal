1ae72e64fc88eecff36034a4febd1bbd74b5fedf
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-07 18:52:28 +0300

    OGGYKIDS-204246: Add properties to class

--- module/Application/src/Model/Entity/Dialog.php
+++ module/Application/src/Model/Entity/Dialog.php
@@ -8,9 +8,49 @@ use Laminas\Hydrator\HydratorInterface;
 class Dialog implements HydratorAwareInterface
 {
     /**
+     * @var int|null
+     */
+    private $id;
+    /**
+     * @var int
+     */
+    private $buddyId;
+    /**
      * @var HydratorInterface
      */
     private $hydrator;
+
+    /**
+     * @param int|null $id
+     * @param int      $buddyId
+     */
+    public function __construct(
+        $buddyId = 0,
+        $id = null
+    ) {
+        $this->id = $id;
+        $this->buddyId = $buddyId;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setBuddyId($buddyId)
+    {
+        $this->buddyId = $buddyId;
+    }
+
+    public function getBuddyId()
+    {
+        return $this->buddyId;
+    }
 
     public function getHydrator(): ?HydratorInterface
     {
