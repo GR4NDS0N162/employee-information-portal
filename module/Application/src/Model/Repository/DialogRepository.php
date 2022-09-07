b219649ac5fda4aa2142b59e2618051f98d59f7d
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-07 18:42:35 +0300

    OGGYKIDS-204246: Create DialogRepository class

---
+++ module/Application/src/Model/Repository/DialogRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Application\Model\Repository;
+
+use Application\Model\Entity\Dialog;
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Hydrator\HydratorAwareInterface;
+
+class DialogRepository implements DialogRepositoryInterface
+{
+    /**
+     * @var AdapterInterface
+     */
+    private $db;
+    /**
+     * @var Dialog|HydratorAwareInterface
+     */
+    private $prototype;
+
+    /**
+     * @param AdapterInterface              $db
+     * @param Dialog|HydratorAwareInterface $prototype
+     */
+    public function __construct(
+        $db,
+        $prototype
+    ) {
+        $this->db = $db;
+        $this->prototype = $prototype;
+    }
+}
