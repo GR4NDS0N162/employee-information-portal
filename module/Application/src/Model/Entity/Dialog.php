c93fffff7967f58a241d9c3c92942bb6ca078577
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-07 19:40:02 +0300

    OGGYKIDS-204246: Initialize the hydrator

--- module/Application/src/Model/Entity/Dialog.php
+++ module/Application/src/Model/Entity/Dialog.php
@@ -2,8 +2,11 @@
 
 namespace Application\Model\Entity;
 
+use Laminas\Hydrator\ClassMethodsHydrator;
 use Laminas\Hydrator\HydratorAwareInterface;
 use Laminas\Hydrator\HydratorInterface;
+use Laminas\Hydrator\Strategy\NullableStrategy;
+use Laminas\Hydrator\Strategy\ScalarTypeStrategy;
 
 class Dialog implements HydratorAwareInterface
 {
@@ -30,6 +33,10 @@ class Dialog implements HydratorAwareInterface
     ) {
         $this->id = $id;
         $this->buddyId = $buddyId;
+
+        $this->hydrator = new ClassMethodsHydrator(false);
+        $this->hydrator->addStrategy('id', new NullableStrategy(ScalarTypeStrategy::createToInt(), true));
+        $this->hydrator->addStrategy('buddyId', ScalarTypeStrategy::createToInt());
     }
 
     public function __get($prop)
