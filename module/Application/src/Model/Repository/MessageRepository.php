cde05f60a0c3e5888cd589f007ca570fb937ed35
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-08 18:22:16 +0300

    OGGYKIDS-204246: Delete the where parameter

--- module/Application/src/Model/Repository/MessageRepository.php
+++ module/Application/src/Model/Repository/MessageRepository.php
@@ -32,11 +32,9 @@ class MessageRepository implements MessageRepositoryInterface
 
     public function findMessagesOfDialog(
         $dialogId,
-        $where = [],
         $limit = null,
         $offset = null
-    )
-    {
+    ) {
         $select = new Select('message');
         $select->columns([
             'id',
--- module/Application/src/Model/Repository/MessageRepositoryInterface.php
+++ module/Application/src/Model/Repository/MessageRepositoryInterface.php
@@ -3,22 +3,18 @@
 namespace Application\Model\Repository;
 
 use Application\Model\Entity\Message;
-use Laminas\Db\Sql\Predicate\PredicateInterface;
-use Laminas\Db\Sql\Where;
 
 interface MessageRepositoryInterface
 {
     /**
-     * @param int                                           $dialogId
-     * @param Where|Closure|string|array|PredicateInterface $where
-     * @param int|null                                      $limit
-     * @param int|null                                      $offset
+     * @param int      $dialogId
+     * @param int|null $limit
+     * @param int|null $offset
      *
      * @return Message[]
      */
     public function findMessagesOfDialog(
         $dialogId,
-        $where = [],
         $limit = null,
         $offset = null
     );
