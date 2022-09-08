39f6daa12d7b406f6d63fd2798d5f78558c60368
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-08 18:01:50 +0300

    OGGYKIDS-204246: Rearrange the code

--- module/Application/config/service_manager.php
+++ module/Application/config/service_manager.php
@@ -5,39 +5,43 @@ namespace Application;
 use Application\Factory\Command as CommandFactory;
 use Application\Factory\Options as OptionsFactory;
 use Application\Factory\Repository as RepositoryFactory;
+use Application\Model\Command as Command;
+use Application\Model\Options as Options;
+use Application\Model\Repository as Repository;
 
 return [
     'aliases'   => [
-        Model\Repository\EmailRepositoryInterface::class      => Model\Repository\EmailRepository::class,
-        Model\Repository\PhoneRepositoryInterface::class      => Model\Repository\PhoneRepository::class,
-        Model\Repository\PositionRepositoryInterface::class   => Model\Repository\PositionRepository::class,
-        Model\Repository\StatusRepositoryInterface::class     => Model\Repository\StatusRepository::class,
-        Model\Repository\UserRepositoryInterface::class       => Model\Repository\UserRepository::class,
-        Model\Repository\UserStatusRepositoryInterface::class => Model\Repository\UserStatusRepository::class,
-        Model\Repository\UserInfoRepositoryInterface::class   => Model\Repository\UserInfoRepository::class,
-        Model\Repository\DialogRepositoryInterface::class     => Model\Repository\DialogRepository::class,
-        Model\Repository\MessageRepositoryInterface::class    => Model\Repository\MessageRepository::class,
-        Model\Command\UserCommandInterface::class             => Model\Command\UserCommand::class,
-        Model\Command\PositionCommandInterface::class         => Model\Command\PositionCommand::class,
-        Model\Command\EmailCommandInterface::class            => Model\Command\EmailCommand::class,
-        Model\Command\PhoneCommandInterface::class            => Model\Command\PhoneCommand::class,
+        Repository\EmailRepositoryInterface::class      => Repository\EmailRepository::class,
+        Repository\PhoneRepositoryInterface::class      => Repository\PhoneRepository::class,
+        Repository\PositionRepositoryInterface::class   => Repository\PositionRepository::class,
+        Repository\StatusRepositoryInterface::class     => Repository\StatusRepository::class,
+        Repository\UserRepositoryInterface::class       => Repository\UserRepository::class,
+        Repository\UserStatusRepositoryInterface::class => Repository\UserStatusRepository::class,
+        Repository\UserInfoRepositoryInterface::class   => Repository\UserInfoRepository::class,
+        Repository\DialogRepositoryInterface::class     => Repository\DialogRepository::class,
+        Repository\MessageRepositoryInterface::class    => Repository\MessageRepository::class,
+
+        Command\UserCommandInterface::class     => Command\UserCommand::class,
+        Command\PositionCommandInterface::class => Command\PositionCommand::class,
+        Command\EmailCommandInterface::class    => Command\EmailCommand::class,
+        Command\PhoneCommandInterface::class    => Command\PhoneCommand::class,
     ],
     'factories' => [
-        Model\Command\UserCommand::class     => CommandFactory\UserCommandFactory::class,
-        Model\Command\PositionCommand::class => CommandFactory\PositionCommandFactory::class,
-        Model\Command\EmailCommand::class    => CommandFactory\EmailCommandFactory::class,
-        Model\Command\PhoneCommand::class    => CommandFactory\PhoneCommandFactory::class,
+        Command\UserCommand::class     => CommandFactory\UserCommandFactory::class,
+        Command\PositionCommand::class => CommandFactory\PositionCommandFactory::class,
+        Command\EmailCommand::class    => CommandFactory\EmailCommandFactory::class,
+        Command\PhoneCommand::class    => CommandFactory\PhoneCommandFactory::class,
 
-        Model\Options\PositionOptions::class => OptionsFactory\PositionOptionsFactory::class,
+        Options\PositionOptions::class => OptionsFactory\PositionOptionsFactory::class,
 
-        Model\Repository\EmailRepository::class      => RepositoryFactory\EmailRepositoryFactory::class,
-        Model\Repository\PhoneRepository::class      => RepositoryFactory\PhoneRepositoryFactory::class,
-        Model\Repository\PositionRepository::class   => RepositoryFactory\PositionRepositoryFactory::class,
-        Model\Repository\StatusRepository::class     => RepositoryFactory\StatusRepositoryFactory::class,
-        Model\Repository\UserRepository::class       => RepositoryFactory\UserRepositoryFactory::class,
-        Model\Repository\UserStatusRepository::class => RepositoryFactory\UserStatusRepositoryFactory::class,
-        Model\Repository\UserInfoRepository::class   => RepositoryFactory\UserInfoRepositoryFactory::class,
-        Model\Repository\DialogRepository::class     => RepositoryFactory\DialogRepositoryFactory::class,
-        Model\Repository\MessageRepository::class    => RepositoryFactory\MessageRepositoryFactory::class,
+        Repository\EmailRepository::class      => RepositoryFactory\EmailRepositoryFactory::class,
+        Repository\PhoneRepository::class      => RepositoryFactory\PhoneRepositoryFactory::class,
+        Repository\PositionRepository::class   => RepositoryFactory\PositionRepositoryFactory::class,
+        Repository\StatusRepository::class     => RepositoryFactory\StatusRepositoryFactory::class,
+        Repository\UserRepository::class       => RepositoryFactory\UserRepositoryFactory::class,
+        Repository\UserStatusRepository::class => RepositoryFactory\UserStatusRepositoryFactory::class,
+        Repository\UserInfoRepository::class   => RepositoryFactory\UserInfoRepositoryFactory::class,
+        Repository\DialogRepository::class     => RepositoryFactory\DialogRepositoryFactory::class,
+        Repository\MessageRepository::class    => RepositoryFactory\MessageRepositoryFactory::class,
     ],
 ];
