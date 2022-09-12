3d90533ecc75dd67657528effcb495996c79e3ac
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-12 22:17:17 +0300

    OGGYKIDS-204246: Get rid of the separate hydrator implementation

--- module/Application/src/Factory/Repository/PhoneRepositoryFactory.php
+++ module/Application/src/Factory/Repository/PhoneRepositoryFactory.php
@@ -12,12 +12,9 @@ class PhoneRepositoryFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
-        $prototype = new Phone();
-
         return new PhoneRepository(
             $container->get(AdapterInterface::class),
-            $prototype->getHydrator(),
-            $prototype,
+            new Phone(),
         );
     }
 }
--- module/Application/src/Model/Repository/PhoneRepository.php
+++ module/Application/src/Model/Repository/PhoneRepository.php
@@ -5,7 +5,6 @@ namespace Application\Model\Repository;
 use Application\Model\Entity\Phone;
 use Laminas\Db\Adapter\AdapterInterface;
 use Laminas\Db\Sql\Select;
-use Laminas\Hydrator\HydratorInterface;
 
 class PhoneRepository implements PhoneRepositoryInterface
 {
@@ -14,23 +13,17 @@ class PhoneRepository implements PhoneRepositoryInterface
      */
     private $db;
     /**
-     * @var HydratorInterface
-     */
-    private $hydrator;
-    /**
      * @var Phone
      */
     private $prototype;
 
     /**
-     * @param AdapterInterface  $db
-     * @param HydratorInterface $hydrator
-     * @param Phone             $prototype
+     * @param AdapterInterface $db
+     * @param Phone            $prototype
      */
-    public function __construct($db, $hydrator, $prototype)
+    public function __construct($db, $prototype)
     {
         $this->db = $db;
-        $this->hydrator = $hydrator;
         $this->prototype = $prototype;
     }
 
@@ -43,6 +36,11 @@ class PhoneRepository implements PhoneRepositoryInterface
         ]);
         $select->where(['user_id = ?' => $userId]);
 
-        return Extracter::extractValues($select, $this->db, $this->hydrator, $this->prototype);
+        return Extracter::extractValues(
+            $select,
+            $this->db,
+            $this->prototype->getHydrator(),
+            $this->prototype
+        );
     }
 }
