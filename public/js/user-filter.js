0e3fd7f55c991c673e39888d6774f0126d1d52a7
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-15 14:51:27 +0300

    OGGYKIDS-204246: Add a data extraction function to display

--- public/js/user-filter.js
+++ public/js/user-filter.js
@@ -2,6 +2,15 @@ const formSelector = $('#AdminFilterForm');
 const sort = $(`#sort`);
 const page = $(`#page`);
 
+function getData()
+{
+    return {
+        where: formSelector.serialize(),
+        page: page[0].value || 1,
+        order: sort[0].value || 'fullname',
+    };
+}
+
 formSelector[0].addEventListener('submit', (e) =>
 {
     e.preventDefault();
