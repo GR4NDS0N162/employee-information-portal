227cc4d20308ce913c81f96f1bbb2b01b1c2d517
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-19 13:37:17 +0300

    OGGYKIDS-204246: Add the code that downloads the messages

---
+++ public/js/message-ajax.js
@@ -0,0 +1,24 @@
+let lastMessageId;
+
+function loadMessages()
+{
+    $.ajax({
+        url: '/user/im/load',
+        method: 'post',
+        dataType: 'html',
+        async: false,
+        data: {
+            lastMessageId: lastMessageId,
+            buddyId: buddyId,
+        },
+    }).done((data) =>
+    {
+        $(`#message-list`)[0].innerHTML += data;
+
+        let lastMessage = $(`#message-list > [message_id]:last-child`);
+
+        if (lastMessage.length) {
+            lastMessageId = parseInt(lastMessage[0].getAttribute('message_id'));
+        }
+    });
+}
