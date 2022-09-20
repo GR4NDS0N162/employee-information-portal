75dce20b50712214cc5c29b017147a9141179f86
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-20 11:13:19 +0300

    OGGYKIDS-204246: Add phpdocs to constants

--- module/Application/src/Controller/UserController.php
+++ module/Application/src/Controller/UserController.php
@@ -13,7 +13,13 @@ use Laminas\View\Model\ViewModel;
 
 class UserController extends AbstractActionController
 {
+    /**
+     * Maximum number of users displayed on a page.
+     */
     public const maxPageCount = 20;
+    /**
+     * The ID of the user who is currently logged in to the system.
+     */
     public const userId = 1;
 
     /**
