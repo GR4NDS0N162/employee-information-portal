a8ca32ea5c2933fe50fbd6d665934ac783bf19d3
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-21 16:45:12 +0300

    OGGYKIDS-205802: Add the return of the generated user id

--- module/Application/src/Model/Command/UserCommand.php
+++ module/Application/src/Model/Command/UserCommand.php
@@ -104,6 +104,8 @@ class UserCommand implements UserCommandInterface
                     $email->getAddress()
                 )
             );
+        } else {
+            return $id;
         }
     }
 
