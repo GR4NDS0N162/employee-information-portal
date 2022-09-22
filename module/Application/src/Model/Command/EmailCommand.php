058daaa2f0d59299a92d7ce12cf4a4e955c97f37
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-22 15:16:02 +0300

    OGGYKIDS-204246: Add execution of sql queries to the database

--- module/Application/src/Model/Command/EmailCommand.php
+++ module/Application/src/Model/Command/EmailCommand.php
@@ -2,7 +2,10 @@
 
 namespace Application\Model\Command;
 
+use Application\Model\Executer;
 use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Sql\Delete;
+use Laminas\Db\Sql\Insert;
 
 class EmailCommand implements EmailCommandInterface
 {
@@ -20,5 +23,18 @@ class EmailCommand implements EmailCommandInterface
 
         $toDelete = array_diff($oldAddressList, $newAddressList);
         $toAdd = array_diff($newAddressList, $oldAddressList);
+
+        $delete = new Delete('email');
+        $delete->where(['address' => $toDelete]);
+        Executer::executeSql($delete, $this->db);
+
+        $insert = new Insert('email');
+        foreach ($toAdd as $address) {
+            $insert->values([
+                'address' => $address,
+                'user_id' => $userId,
+            ]);
+            Executer::executeSql($insert, $this->db);
+        }
     }
 }
