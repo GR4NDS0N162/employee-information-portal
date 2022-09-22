86453fa0d8de29b9e7126b36d6c4c2ba77c4e26e
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-22 14:49:18 +0300

    OGGYKIDS-204246: Calculate arrays describing the change in the list of emails

--- module/Application/src/Model/Command/EmailCommand.php
+++ module/Application/src/Model/Command/EmailCommand.php
@@ -28,6 +28,9 @@ class EmailCommand implements EmailCommandInterface
         $oldAddressList = array_column($oldList, 'address');
         $newAddressList = array_column($newList, 'address');
 
+        $toDelete = array_diff($oldAddressList, $newAddressList);
+        $toAdd = array_diff($newAddressList, $oldAddressList);
+
         foreach ($oldAddressList as $address) {
             if (!in_array($address, $newAddressList)) {
                 $this->deleteEmailByAddress($address);
