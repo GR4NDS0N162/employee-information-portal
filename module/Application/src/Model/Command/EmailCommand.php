5a98bb669bcfd5b2eae65801dcfeab98df82d9d1
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-22 14:51:45 +0300

    OGGYKIDS-204246: Remove the deleteEmailByAddress() and addEmail() functions and their use in loops

--- module/Application/src/Model/Command/EmailCommand.php
+++ module/Application/src/Model/Command/EmailCommand.php
@@ -2,11 +2,7 @@
 
 namespace Application\Model\Command;
 
-use Application\Model\Entity\Email;
-use Application\Model\Executer;
 use Laminas\Db\Adapter\AdapterInterface;
-use Laminas\Db\Sql\Delete;
-use Laminas\Db\Sql\Insert;
 
 class EmailCommand implements EmailCommandInterface
 {
@@ -30,36 +26,5 @@ class EmailCommand implements EmailCommandInterface
 
         $toDelete = array_diff($oldAddressList, $newAddressList);
         $toAdd = array_diff($newAddressList, $oldAddressList);
-
-        foreach ($oldAddressList as $address) {
-            if (!in_array($address, $newAddressList)) {
-                $this->deleteEmailByAddress($address);
-            }
-        }
-
-        foreach ($newAddressList as $address) {
-            if (!in_array($address, $oldAddressList)) {
-                $this->addEmail(new Email($address, $userId));
-            }
-        }
-    }
-
-    private function deleteEmailByAddress($address)
-    {
-        $delete = new Delete('email');
-        $delete->where(['address = ?' => $address]);
-
-        Executer::executeSql($delete, $this->db);
-    }
-
-    private function addEmail($email)
-    {
-        $insert = new Insert('email');
-        $insert->values([
-            'user_id' => $email->getUserId(),
-            'address' => $email->getAddress(),
-        ]);
-
-        Executer::executeSql($insert, $this->db);
     }
 }
