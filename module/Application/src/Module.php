d8b193b5695684778b74020743607df0c39d17d0
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-27 15:11:31 +0300

    OGGYKIDS-204246: Register JsonStrategy

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -2,10 +2,28 @@
 
 namespace Application;
 
+use Laminas\Mvc\MvcEvent;
+
 class Module
 {
     public function getConfig(): array
     {
         return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        $app = $e->getApplication();
+        $app->getEventManager()->attach('render', [$this, 'registerJsonStrategy'], 100);
+    }
+
+    public function registerJsonStrategy(MvcEvent $e)
+    {
+        $app = $e->getTarget();
+        $locator = $app->getServiceManager();
+        $view = $locator->get('Laminas\View\View');
+        $jsonStrategy = $locator->get('ViewJsonStrategy');
+
+        $jsonStrategy->attach($view->getEventManager(), 100);
     }
 }
