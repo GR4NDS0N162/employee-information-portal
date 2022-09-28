81450df1f68f952e4c3790a7a6bc20aecf7feeaf
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-28 21:48:44 +0300

    OGGYKIDS-205802: Remove argument

--- module/Application/src/Controller/MessengerController.php
+++ module/Application/src/Controller/MessengerController.php
@@ -115,7 +115,7 @@ class MessengerController extends AbstractActionController
             throw new LogicException('The request to the address must be ajax and post.');
         }
 
-        $data = ConfigHelper::configWhereData($request->getContent(), true);
+        $data = ConfigHelper::configWhereData($request->getContent());
 
         $viewModel = new ViewModel();
         $viewModel->setTerminal(true);
