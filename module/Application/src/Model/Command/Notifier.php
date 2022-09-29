e9ea9b1e168f318ba0888b11faca35accb1a745e
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-29 16:57:28 +0300

    OGGYKIDS-205802: Remove unused methods and properties

--- module/Application/src/Model/Command/Notifier.php
+++ module/Application/src/Model/Command/Notifier.php
@@ -2,22 +2,11 @@
 
 namespace Application\Model\Command;
 
-use Application\Model\Entity\Email;
-use Laminas\Db\Adapter\AdapterInterface;
 use Laminas\Mail\Message;
 use Laminas\Mail\Transport\Sendmail;
 
 class Notifier implements NotifierInterface
 {
-    private AdapterInterface $db;
-    private Email $prototype;
-
-    public function __construct(AdapterInterface $db)
-    {
-        $this->db = $db;
-        $this->prototype = new Email();
-    }
-
     public function sendEmails(array $mailsInfo)
     {
         $transport = new Sendmail();
