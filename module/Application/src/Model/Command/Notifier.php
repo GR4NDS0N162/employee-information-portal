bbc9e397f22a00cc0798a8862b11fb62e3bbbd85
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-29 16:55:39 +0300

    OGGYKIDS-205802: Remove getting data from the database from the method

--- module/Application/src/Model/Command/Notifier.php
+++ module/Application/src/Model/Command/Notifier.php
@@ -23,37 +23,6 @@ class Notifier implements NotifierInterface
 
     public function sendEmails(array $mailsInfo)
     {
-        $select = new Select(['mes' => 'message']);
-        $select->columns([
-            'address' => new Expression('MIN(e.address)'),
-            'userId'  => 'mes.user_id',
-        ], false);
-        $select->join(
-            ['mem' => 'member'],
-            new Expression(
-                'mes.dialog_id = mem.dialog_id ' .
-                'AND mes.user_id != mem.user_id'
-            ),
-            []
-        );
-        $select->join(
-            ['e' => 'email'],
-            'mem.user_id = e.user_id',
-            []
-        );
-        $select->where([
-            'mes.id' => array_column($messages, 'id'),
-        ]);
-        $select->group(['e.user_id', 'mes.user_id']);
-
-        /** @var Email[] $mailsInfo */
-        $mailsInfo = Extracter::extractValues(
-            $select,
-            $this->db,
-            $this->prototype->getHydrator(),
-            $this->prototype
-        );
-
         $transport = new Sendmail();
 
         foreach ($mailsInfo as $email) {
