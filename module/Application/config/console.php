a4fde13283f57d1d656c772c1d4ff1872d33a481
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-29 11:17:49 +0300

    OGGYKIDS-205802: Remove one level from the array

--- module/Application/config/console.php
+++ module/Application/config/console.php
@@ -5,16 +5,14 @@ namespace Application;
 use Application\Controller\MessengerController;
 
 return [
-    'console' => [
-        'router' => [
-            'routes' => [
-                'send-emails' => [
-                    'options' => [
-                        'route'    => 'send-emails',
-                        'defaults' => [
-                            'controller' => MessengerController::class,
-                            'action'     => 'send-emails',
-                        ],
+    'router' => [
+        'routes' => [
+            'send-emails' => [
+                'options' => [
+                    'route'    => 'send-emails',
+                    'defaults' => [
+                        'controller' => MessengerController::class,
+                        'action'     => 'send-emails',
                     ],
                 ],
             ],
