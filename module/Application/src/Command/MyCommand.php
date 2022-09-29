ab6f7482252b930dae320dde5375a7776bac5480
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-29 13:14:58 +0300

    OGGYKIDS-205802: Implement dependency on MessageRepositoryInterface

--- module/Application/src/Command/MyCommand.php
+++ module/Application/src/Command/MyCommand.php
@@ -2,12 +2,24 @@
 
 namespace Application\Command;
 
+use Application\Model\Repository\MessageRepositoryInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class MyCommand extends Command
 {
+    private MessageRepositoryInterface $messageRepository;
+
+    public function __construct(
+        MessageRepositoryInterface $messageRepository,
+        string                     $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->messageRepository = $messageRepository;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // TODO: Implement execute() method.
--- module/Application/src/Command/MyCommandFactory.php
+++ module/Application/src/Command/MyCommandFactory.php
@@ -2,6 +2,7 @@
 
 namespace Application\Command;
 
+use Application\Model\Repository\MessageRepositoryInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Psr\Container\ContainerInterface;
 
@@ -9,6 +10,8 @@ class MyCommandFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
-        return new MyCommand();
+        return new MyCommand(
+            $container->get(MessageRepositoryInterface::class),
+        );
     }
 }
