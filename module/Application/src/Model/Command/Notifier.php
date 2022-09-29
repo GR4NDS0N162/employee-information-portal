51f572280b17fc2a8679964673e29c2cee687e41
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-29 13:52:09 +0300

    OGGYKIDS-205802: Create Notifier class

---
+++ module/Application/src/Model/Command/Notifier.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Application\Model\Command;
+
+class Notifier implements NotifierInterface
+{
+    public function sendEmails(array $messages)
+    {
+        // TODO: Implement sendEmails() method.
+    }
+}
