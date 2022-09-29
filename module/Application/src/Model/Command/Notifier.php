f6c8899b14a106207f5e8d2e79bcc15ad952cf47
Kirill Vnukov <kirillgrandson@gmail.com>
2022-09-29 15:10:24 +0300

    OGGYKIDS-205802: Form a select with information to start sending messages

--- module/Application/src/Model/Command/Notifier.php
+++ module/Application/src/Model/Command/Notifier.php
@@ -3,6 +3,8 @@
 namespace Application\Model\Command;
 
 use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Sql\Predicate\Expression;
+use Laminas\Db\Sql\Select;
 
 class Notifier implements NotifierInterface
 {
@@ -15,6 +17,27 @@ class Notifier implements NotifierInterface
 
     public function sendEmails(array $messages)
     {
-        // TODO: Implement sendEmails() method.
+        $select = new Select(['mes' => 'message']);
+        $select->columns([
+            'address' => new Expression('MIN(e.address)'),
+            'userId'  => 'mes.user_id',
+        ], false);
+        $select->join(
+            ['mem' => 'member'],
+            new Expression(
+                'mes.dialog_id = mem.dialog_id ' .
+                'AND mes.user_id != mem.user_id'
+            ),
+            []
+        );
+        $select->join(
+            ['e' => 'email'],
+            'mem.user_id = e.user_id',
+            []
+        );
+        $select->where([
+            'mes.id' => array_column($messages, 'id'),
+        ]);
+        $select->group(['e.user_id']);
     }
 }
